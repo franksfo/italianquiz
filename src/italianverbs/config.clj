083fed6879e99f03c3fcd45fe385026b724bff0b
Eugene Koontz <ekoontz@hiro-tan.org>
2011-05-30 23:47:15 -0700

    show more config options commented-out.

--- src/italianverbs/config.clj
+++ src/italianverbs/config.clj
@@ -1,7 +1,13 @@
 ;; RESTARTING OF RING NOT REQUIRED FOR CHANGES TO THIS FILE.
 (ns italianverbs.config)
 
-;; examples of constraints 
+
+;; which tests to run : set to true to run.
+(def sentence false)
+(def present true)
+(def passato false)
+
+;; examples of constraints on generation.
 
 (def random-passato-prossimo-verb-past
   {
@@ -10,12 +16,15 @@
 ;   :italian "stato"
    })
 
-
-
-  
-
 (def random-passato-prossimo-subj
   {
+   })
+
+(def random-present-verb
+  {
+;   :person :3rd
+;   :number :singular
+;   :root.english "to offer"
    })
 
 
