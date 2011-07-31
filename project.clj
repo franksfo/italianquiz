c9ecf636f25727d04e0d139b12c22cd20b029a42
Eugene Koontz <ekoontz@hiro-tan.org>
2011-07-31 10:45:20 -0700

    upgrading clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject italianverbs "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [slice "0.4.0-SNAPSHOT"]
                  [org.apache.lucene/lucene-core "3.0.2"]
