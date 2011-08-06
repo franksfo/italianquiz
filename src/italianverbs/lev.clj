7995bb1b2198afb60e12bd100f72c69e3678c5bc
Eugene Koontz <ekoontz@hiro-tan.org>
2011-08-06 16:53:52 -0700

    move (get-min) to within (add-one-row)

--- src/italianverbs/lev.clj
+++ src/italianverbs/lev.clj
@@ -8,35 +8,34 @@
   "abc => (\"a\" \"b\" \"c\")"
   (rest (string/split (java.util.regex.Pattern/compile "") string)))
 
-(defn get-min [matrix x y char-x char-y]
-  (let [diag
-        (if (= char-x char-y) 0 1)]
-    (min (if (> x 0)
-           (+ 1 (get matrix (list (- x 1) y)))
-           Float/POSITIVE_INFINITY)
-         (if (> y 0)
-           (+ 1 (get matrix (list x (- y 1))))
-           Float/POSITIVE_INFINITY)
-         (if (and (> x 0) (> y 0))
-           (+ diag
-              (get matrix (list (- x 1) (- y 1))))
-           Float/POSITIVE_INFINITY))))
-
 (defn add-one-row [matrix x y horiz-char-list vert-char-list]
   "adds values for one whole row ([x:[0,end], y]) to the matrix."
-  (if (get matrix (list x (- y 1)))
-    (add-one-row
-     (merge
-      matrix
-      {(list x y) (get-min matrix
-                           x y
-                           (nth horiz-char-list x)
-                           (nth vert-char-list y)
-                           )})
-     (+ 1 x) y
-     horiz-char-list vert-char-list)
-    matrix))
-
+  (let [get-min (fn [matrix x y char-x char-y]
+                  (let [diag
+                        (if (= char-x char-y) 0 1)]
+                    (min (if (> x 0)
+                           (+ 1 (get matrix (list (- x 1) y)))
+                           Float/POSITIVE_INFINITY)
+                         (if (> y 0)
+                           (+ 1 (get matrix (list x (- y 1))))
+                           Float/POSITIVE_INFINITY)
+                         (if (and (> x 0) (> y 0))
+                           (+ diag
+                              (get matrix (list (- x 1) (- y 1))))
+                           Float/POSITIVE_INFINITY))))]
+    (if (get matrix (list x (- y 1)))
+      (add-one-row
+       (merge
+        matrix
+        {(list x y) (get-min matrix
+                             x y
+                             (nth horiz-char-list x)
+                             (nth vert-char-list y)
+                             )})
+       (+ 1 x) y
+       horiz-char-list vert-char-list)
+      matrix)))
+  
 (defn create-matrix [matrix j y horiz-char-list vert-char-list]
   "adds one row at a time to matrix for all rows up to y.
    assumes that matrix already has one row created with
@@ -77,30 +76,30 @@
     (str "<th>" (first charlist) "</th>"
          (matrix-header (rest charlist)))))
 
+(defn tablize-row [matrix i j horiz-char-list vert-char-list]
+  (if (get matrix (list i j))
+    (str
+     "<td>"
+     (get matrix (list i j))
+     "</td>"
+     (tablize-row matrix (+ i 1) j horiz-char-list vert-char-list))
+    ""))
+
 (defn tablize [matrix horiz-char-list vert-char-list j]
-  (let [tablize-row
-        (fn [matrix i j horiz-char-list vert-char-list]
-          (if (get matrix (list i j))
-            (str
-             "<td>"
-             (get matrix (list i j))
-             "</td>"
-             (tablize-row matrix (+ i 1) j horiz-char-list vert-char-list))
-            ""))]
-    (if (get matrix (list 0 j))
-      (str
-       "<tr>"
-       "<th>"
-       j
-       "</th>"
-       "<th>"
-       ;; j'th element of vertical string.
-       (nth vert-char-list j)
-       "</th>"
-       (tablize-row matrix 0 j horiz-char-list vert-char-list)
-       "</tr>"
-       (tablize matrix horiz-char-list vert-char-list (+ 1 j)))
-      "")))
+  (if (get matrix (list 0 j))
+    (str
+     "<tr>"
+     "<th>"
+     j
+     "</th>"
+     "<th>"
+     ;; j'th element of vertical string.
+     (nth vert-char-list j)
+     "</th>"
+     (tablize-row matrix 0 j horiz-char-list vert-char-list)
+     "</tr>"
+     (tablize matrix horiz-char-list vert-char-list (+ 1 j)))
+    ""))
 
 (defn interpret-op [op curr diag horiz-char-list vert-char-list x y]
   (if (= op nil)
