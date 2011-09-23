58d5a2a9af4b6879b41e23187959f4b42df02453
Eugene Koontz <ekoontz@hiro-tan.org>
2011-09-23 09:43:38 -0700

    more comments about REPL usage

--- src/italianverbs/repl.clj
+++ src/italianverbs/repl.clj
@@ -2,15 +2,19 @@
 ; $ lein repl
 ; user=> (load "italianverbs/repl")
 ; user=> (ns italianverbs.generate)
-; italianverbs.generate=> (load "generate")
 ; ;;;; namespace warnings about the 'test' symbol..
 ; italianverbs.generate=> (random-futuro-semplice)
 ; {:root {:infl "infinitive", :cat "verb", :subj ...
 ; ..etc
+; ;;; to use italianverbs.generate/test, you must explicitly load "generate" again:
+; italianverbs.generate=> (load "generate")
+; italianverbs.generate=> (ns italianverbs.generate)
+; italianverbs.generate=> (test)
+; ..test output..
 
 (ns italianverbs.generate
   (:use [somnium.congomongo]))
 
 (mongo! :db "mydb")
-
+(load "generate")
 
