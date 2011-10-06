2993c5193e9ff6ffbd315da5382d0b6e5ae2df1b
Eugene Koontz <ekoontz@hiro-tan.org>
2011-10-05 22:45:17 -0700

    fix table updating

--- resources/public/js/quiz.js
+++ resources/public/js/quiz.js
@@ -1,15 +1,15 @@
 var staticContentRow = 1;
-var guessNumber = 1;
 
 function got(here) {
-    $("#guessbody").prepend("<tr style='display:none' id='row_" + staticContentRow + "'><th>"+staticContentRow+"</th><th>got</th><td>" + here + "</td></tr>");
-    $("#row_"+staticContentRow).fadeIn("slow");
+    $("#guessbody").prepend("<tr style='display:none' id='got_row_" + staticContentRow + "'><th>"+staticContentRow+"</th><th>got</th><td>" + here + "</td></tr>");
+    $("#got_row_"+staticContentRow).fadeIn("slow");
     staticContentRow++;
 }
 
+var guessNumber = 1;
+
 function addguess(english,italian) {
-    $("#guess-table").prepend("<tr style='display:none' id='row_" + guessNumber + "'><th>" + guessNumber + "</th><th>" + english + "</th><td>" + italian + "</td></tr>");
-    $("#row_"+guessNumber).fadeIn("slow");
+    $("#guess-table").prepend("<tr style='display:none' id='guess_row_" + guessNumber + "'><th>" + guessNumber + "</th><th>" + english + "</th><td>" + italian + "</td></tr>");
+    $("#guess_row_"+guessNumber).fadeIn("slow");
     guessNumber++;
 }
-
