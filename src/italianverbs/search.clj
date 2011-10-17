0d4c30ea9757ae919aa626082c4849602edb2304
Eugene Koontz <ekoontz@hiro-tan.org>
2011-10-16 20:01:28 -0700

    working on search.

--- src/italianverbs/search.clj
+++ src/italianverbs/search.clj
@@ -2,32 +2,17 @@
     (:require [italianverbs.lexicon :as lexicon]
               [italianverbs.lev :as lev]
               [italianverbs.grammar :as gram]
-              [italianverbs.html :as html] ;tmp hopefully; html package should be able to handle retval type of gram/choose-lexeme.
               [italianverbs.morphology :as morph]))
 
-;; sadly cannot pass params yet.
-
-(defn search-verbs []
-  (gram/choose-lexeme {:cat :verb}))
-
-(defn search-nouns []
-  (gram/choose-lexeme {:cat :noun}))
-
-
-(defn search [& constraints]
-  (if constraints
-                                        ;    (gram/choose-lexeme {:cat :verb})
-    ; below does not work for some reason:
-                                        ;    (gram/choose-lexeme constraints)
-  (gram/choose-lexeme)))
-
+(defn search [constraints]
+  (gram/choose-lexeme constraints))
 
 (defn test []
   (list
      {:comment "show the first (database's choice) noun."
-      :test (search-nouns)}
+      :test (search {:cat :noun})}
      {:comment "show the first (database's choice) verb."
-      :test (search-verbs)}))
+      :test (search {:cat :verb})}))
 
 
 
