d9b52b612f478f4b9fa12592e4be54a3b6b0a96c
Eugene Koontz <ekoontz@hiro-tan.org>
2011-10-19 20:29:22 -0700

    cleanup: remove scaffolding

--- resources/public/js/quiz.js
+++ resources/public/js/quiz.js
@@ -1,13 +1,3 @@
-var staticContentRow = 1;
-
-function got(here) {
-    $("#guessbody").prepend("<tr style='display:none' id='got_row_" + staticContentRow + "'><th>"+staticContentRow+"</th><th>got</th><td>" + here + "</td></tr>");
-    $("#got_row_"+staticContentRow).fadeIn("slow");
-    staticContentRow++;
-}
-
-var guessNumber = 1;
-
 function addguess(english,italian) {
     $("#guess-table").prepend("<tr style='display:none' id='guess_row_" + guessNumber + "'><th>" + guessNumber + "</th><th>" + english + "</th><td>" + italian + "</td></tr>");
     $("#guess_row_"+guessNumber).fadeIn("slow");
