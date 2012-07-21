51f23219c32be4ff2bb7f824da5c5c406458be71
Eugene Koontz <ekoontz@hiro-tan.org>
2012-07-21 13:28:22 -0700

    finished transitioning lexiconfn from rdutest to clojure-test

--- test/italianverbs/test/lexiconfn.clj
+++ test/italianverbs/test/lexiconfn.clj
@@ -8,85 +8,67 @@
 ;; TODO: for testing, consider some kind of mongodb mocking of some kind,
 ;; so that no actual mongodb connection would be necessary.
 
-(deftest foo
-  (is (= true true)))
-
-(def tests
-  (list
-   (rdutest
-    "A lexical entry for the word: 'parlare'."
-    (let [verb {:cat :verb}
-          human-subj {:subj {:human true}}
-          third-sing {:subj {:number :singular :person :3rd}}
-          parlare (add "parlare" "to speak"
-                       verb
-                       human-subj
-                       {
-                        :obj {:speakable true}
-                        })
-          merge (fs/merge
-                 parlare
-                 third-sing
-                 {:root parlare})
-          parla (add "parla" "speaks"
+(deftest parlare ;; A lexical entry for the word: 'parlare'
+  (let [merge-and-parla
+        (let [verb {:cat :verb}
+              human-subj {:subj {:human true}}
+              third-sing {:subj {:number :singular :person :3rd}}
+              parlare (add "parlare" "to speak"
+                           verb
+                           human-subj
+                           {
+                            :obj {:speakable true}
+                            })
+              merge (fs/merge
                      parlare
                      third-sing
-                     {:root parlare})]
-      {:merge merge
-       :parla parla})
-    (fn [merge-and-parla]
-      (let [merge (:merge merge-and-parla)
-            parla (:parla merge-and-parla)]
-        (and
-         (= (:cat parla) :verb)
-         (= (get-in parla (list :subj :human)) true)
+                     {:root parlare})
+              parla (add "parla" "speaks"
+                         parlare
+                         third-sing
+                         {:root parlare})]
+          {:merge merge
+           :parla parla})]
+    (let [merge (:merge merge-and-parla)
+          parla (:parla merge-and-parla)]
+      (is (= (:cat parla) :verb))
+      (is (= (get-in parla (list :subj :human)) true)
                                         ;         (= (fs/get-path parla (list :root :subj :human)) true)
                                         ;         (= (fs/get-path parla (list :subj :number)) :singular)
                                         ;         (= (fs/get-path parla (list :subj :person)) :3rd)
                                         ;         (= (fs/get-path parla (list :obj :speakable)) true)
                                         ;         (= (:english parla) "speaks")
                                         ;         (= (:italian parla) "parla")
-         )))
-    :parla)
-   (rdutest
-    "calcio (no complement for this noun)"
-    (let [third-sing {:number :singular
-                      :person :3rd
-                      :cat :noun}
-          common-noun {:comp {:cat :det
-                              :number (:number third-sing);; TODO: use reference.
-                              }
-                       }
-          masc {:gender :masc}
-          mass {:mass true :comp {:def {:not :indef}}} ; you can say 'the pasta', but not 'a pasta'.
-          nil-complement {:comp :nil! :sport true}
-          featuremaps (list common-noun masc mass nil-complement)]
-      {
-       :copied-list (concat (map #'fs/copy featuremaps))
-       :with-apply (apply fs/merge
-                     (concat (map #'fs/copy featuremaps)))
-       })
-    (fn [result]
-      (or (nil? (:comp result))
-          (= {} (:comp result))))
-    :lexiconfn-calcio)
+          ))))
 
-   (rdutest
-    "order of arguments containing references should not matter."
-    (let [agr (ref :top)]
-      (add "letto1" "bed1"
-           {:gender :masc}
-           {:gender agr}))
-    (fn [result]
-      (and (= (type (:gender result)) clojure.lang.Ref)
-           (= @(:gender result) :masc)))
-    :argument-order-of-add)
+(deftest calcio ;; No article for the noun "calcio" (soccer).
+  (let [result
+        (let [third-sing {:number :singular
+                          :person :3rd
+                          :cat :noun}
+              common-noun {:comp {:cat :det
+                                  :number (:number third-sing);; TODO: use reference.
+                                  }
+                           }
+              masc {:gender :masc}
+              mass {:mass true :comp {:def {:not :indef}}} ; you can say 'the pasta', but not 'a pasta'.
+              nil-complement {:comp :nil! :sport true}
+              featuremaps (list common-noun masc mass nil-complement)]
+          {
+           :copied-list (concat (map #'fs/copy featuremaps))
+           :with-apply (apply fs/merge
+                              (concat (map #'fs/copy featuremaps)))
+           })]
+    (is (or (nil? (:comp result))
+            (= {} (:comp result))))))
 
-))
-
-;(def parla
-;  (:test-result ((:3s tests) tests)))
-
-;;usage : (run-query (pathify trans-verbs)))
+(deftest order-independence ;; Order of arguments containing references should not matter.
+  (let [result
+        (let [agr (ref :top)]
+          (add "letto1" "bed1"
+               {:gender :masc}
+               {:gender agr}))]
+    (is (= (type (:gender result)) clojure.lang.Ref))
+    (is (= @(:gender result) :masc))))
 
 
