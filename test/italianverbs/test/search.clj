e5d9097ac5d3611927029c59b7cdb927b5e0c542
Eugene Koontz <ekoontz@hiro-tan.org>
2012-07-22 12:34:15 -0700

    transitioning italianverbs.search to clojure-test

--- test/italianverbs/test/search.clj
+++ test/italianverbs/test/search.clj
@@ -7,112 +7,106 @@
    [italianverbs.fs :as fs]
    [italianverbs.lexiconfn :as lexfn]))
 
-(deftest test-lazy-query
+;; TODO: move some of these lexically-related tests to lexicon.clj (e.g. the 'fare' (to do) test).
+(deftest lazy-query-verb
   (let [result
         (take 1 (lazy-query {:cat :verb}))]
-    (> (.size result) 0)))
-  
-;; TODO: move some of these lexically-related tests to lexicon.clj (e.g. the 'fare' (to do) test).
+    (is (> (.size result) 0))))
+
+(deftest lazy-query-noun
+  (let [result
+        (take 1 (lazy-query {:cat :noun}))]
+    (is (> (.size result) 0))))
+
+(deftest lazy-query-noun-nom
+  (let [result
+        (take 1 (lazy-query {:cat :noun :case :nom}))]
+    (is (> (.size result) 0))))
+
+(deftest lookup-ignores-some-features ;; Features that should be ignored for lookup purposes are in fact ignored.
+  (let [result
+        (take 1 (lazy-query {:cat :noun :case :nom :ref '("foo" "bar") :refmap {:baz 42}}))]
+    (is (> (.size result) 0))))
+
+;;The intersection of mutually-exclusive queries is the null set
+;;(since a noun can't be both nominative and non-nominative).
+(deftest lookup-contradictory-features
+  (let [result (intersection (query {:cat :noun :case :nom}) (query {:cat :noun :case {:not :nom}}))]
+    (is (= (.size result) 0))))
+
+;; There's at least one verb that takes an edible object (a nested query works).
+(deftest verb-with-edible-object
+  (let [result (take 1 (lazy-query {:cat :verb :obj {:edible true}}))]
+    (is (> (.size result) 0))))
+
+;; Looking up an irregular verb inflection by its root works.
+;; (i.e. Look up a word by its root: find a verb whose root is 'fare (to make)' (e.g. 'facio (i make)'))
+(deftest lookup-irregular-inflection-by-root
+  (let [result (search {:root {:italian "fare"}})]
+    (is (not (= result nil)))
+    (is (> (.size result) 0))))
+
+(deftest lookup-irregular-infinitive
+ (let [result (search {:root (first (search {:italian "fare" :cat :verb :infl :infinitive}))})]
+   (is (not (= result nil)))
+   (is (> (.size result) 0))))
+
+;; Search should take multiple parameters and merge them together.
+(deftest search-with-multiple-arguments
+  (let [result (search {:cat :noun} {:gender :fem} {:number :singular})]
+    (is (not (= result nil)))
+    (is (> (.size result) 0))))
+   
+;; "pathifying" a map means flattening a tree into a
+;; list of path-value pairs.
+;;
+;;for example:
+;;
+;; {:root {:gender :masc
+;;         :human true
+;;         :det {:cat :det
+;;               :def true}
+;;         :animate true
+;;         :morph "morph-noun"
+;;         :common true
+;;         :cat :noun
+;;         :italian "uomo"}
+;;  :italian "uomini"
+;;  :number :plural}
+;;
+;; => ({(:root :gender) :masc}
+;;     {(:root :human) true}
+;;     {(:root :det :cat) :det}
+;;     {(:root :det :def) true}
+;;     {(:root :animate) true}
+;;     {(:root :morph) "morph-noun"}
+;;     {(:root :common) true}
+;;     {(:root :cat) :noun}
+;;     {(:root :italian) "uomo"}
+;;     {(:italian) "uomini"}
+;;     {(:number) :plural})
+
+;; Pathify a large map.
+(deftest pathify-map
+  (let [result 
+        (pathify
+         {:root {:gender :masc
+                 :human true
+                 :det {:cat :det
+                       :def true}
+                 :animate true
+             :morph "morph-noun"
+                 :common true
+                 :cat :noun
+                 :italian "uomo"}
+          :italian "uomini"
+          :number :plural})]
+    ;; TODO: write better test with more detailed tests.
+    (is (= (count result) 11)))) 
+
 (def tests
   (list
 
-   (rdutest
-    "At least one verb is in the lexicon."
-    (take 1 (lazy-query {:cat :verb}))
-    #(> (.size %) 0)
-    :one-verb)
-   
-   (rdutest
-    "At least one noun is in the lexicon."
-    (take 1 (lazy-query {:cat :noun}))
-    #(> (.size %) 0)
-    :one-noun)
-   
-   (rdutest
-    "At least one nominative noun is in the lexicon (and that the conjunction of more than one predicate works."
-    (take 1 (lazy-query {:cat :noun :case :nom}))
-    #(> (.size %) 0)
-    :one-nom-noun)
-
-   (rdutest
-    "Features that should be ignored for lookup purposes are in fact ignored."
-    (take 1 (lazy-query {:cat :noun :case :nom :ref '("foo" "bar") :refmap {:baz 42}}))
-    #(> (.size %) 0)
-    :ignore-non-lookup-features)
-
-   
-   (rdutest
-    "The intersection of mutually-exclusive queries is the null set (since a noun can't be both nominative and non-nominative)."
-    (intersection (query {:cat :noun :case :nom}) (query {:cat :noun :case {:not :nom}}))
-    #(= (.size %) 0)
-    :null-set)
-   
-   (rdutest
-    "There's at least one verb that takes an edible object (a nested query works)."
-    (take 1 (lazy-query {:cat :verb :obj {:edible true}}))
-    #(> (.size %) 0)
-    :verb-with-edible-object)
-   
-   (rdutest
-    "Looking up an irregular verb inflection by its root works."
-    (search {:root {:italian "fare"}})
-    #(and (not (= % nil)) (> (.size %) 0))
-    :lookup-roots)
-
-   (rdutest
-    "Look up a word by its root: find a verb whose root is 'fare (to make)' (e.g. 'facio (i make)')."
-    (search {:root (first (search {:italian "fare" :cat :verb :infl :infinitive}))})
-    #(and (not (= % nil)) (> (.size %) 0))
-    :lookup-complex-root)
-   
-   (rdutest
-    "Search should take multiple parameters and merge them together."
-    (search {:cat :noun} {:gender :fem} {:number :singular})
-    #(and (not (= % nil)) (> (.size %) 0))
-    :search-multiple-maps)
-   
-   ;; "pathifying" a map means (by example)
-   ;;
-   ;; {:root {:gender :masc
-   ;;         :human true
-   ;;         :det {:cat :det
-   ;;               :def true}
-   ;;         :animate true
-   ;;         :morph "morph-noun"
-   ;;         :common true
-   ;;         :cat :noun
-   ;;         :italian "uomo"}
-   ;;  :italian "uomini"
-   ;;  :number :plural}
-   ;;
-   ;; => ({(:root :gender) :masc}
-   ;;     {(:root :human) true}
-   ;;     {(:root :det :cat) :det}
-   ;;     {(:root :det :def) true}
-   ;;     {(:root :animate) true}
-   ;;     {(:root :morph) "morph-noun"}
-   ;;     {(:root :common) true}
-   ;;     {(:root :cat) :noun}
-   ;;     {(:root :italian) "uomo"}
-   ;;     {(:italian) "uomini"}
-   ;;     {(:number) :plural})
-
-   (rdutest
-    "Pathify a large map."
-    (pathify
-     {:root {:gender :masc
-             :human true
-             :det {:cat :det
-                   :def true}
-             :animate true
-             :morph "morph-noun"
-             :common true
-             :cat :noun
-             :italian "uomo"}
-      :italian "uomini"
-      :number :plural})
-    (fn [paths] (= (count paths) 11)) ;; TODO: write better test.
-    :pathify-long-map-1)
    
    (rdutest
     "Pathify another map."
