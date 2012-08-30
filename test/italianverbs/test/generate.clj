97ab39f33fa3febb9e2fa8f1864a6eb3e8ce58af
Eugene Koontz <ekoontz@hiro-tan.org>
2012-08-30 09:49:06 -0700

    working on generate tests

--- test/italianverbs/test/generate.clj
+++ test/italianverbs/test/generate.clj
@@ -3,7 +3,9 @@
         [italianverbs.generate])
   (:require
    [somnium.congomongo :as mongo]
-   [italianverbs.fs :as fs]))
+   [italianverbs.fs :as fs]
+   [italianverbs.lexiconfn :as lexfn]
+   [italianverbs.search :as search]))
 
 (deftest t1
   (let [ref3 (ref :top)
@@ -16,9 +18,10 @@
         irreg-vp
         {:a ref1
          :b {:italian ref2
-             :root {:infl ref4
-                    :passato-prossimo ref2
-                    :passato-prossimo-aux ref1}}
+             :root {:infl :infinitive
+                    :passato-prossimo {:italian ref2}
+                    ;; a) note: no italian here: compare to b) below.
+                    :passato-prossimo-aux ref1}} 
          :italian {:a ref3
                    :b ref2}
          :infl ref4}
@@ -32,11 +35,12 @@
          :passato-prossimo {:italian "fatto"}}
 
         unified
-        (fs/unify irreg-vp
-                  {:b {:root fare}})]
+        (fs/unify (fs/copy irreg-vp)
+                  {:b {:root (fs/copy fare)}})]
 
     ;; TODO: more tests.
-    (is (= (fs/get-in unified '(:a :italian)) "avere"))))
+    (is (= (fs/get-in unified '(:a :italian)) "avere"))
+    (printfs (list irreg-vp {:b {:root fare}} unified) "fare.html")))
 
 (deftest t2
   (let [ref3 (ref :top)
@@ -51,8 +55,9 @@
         {:a ref1
          :b {:italian ref2
              :infl :passato-prossimo
-             :root {:infl ref5
-                    :italian ref4
+             :root {:infl :infinitive
+                    ;; b) note: italian here.
+                    :italian ref4 
                     :passato-prossimo-aux ref1}}
          :italian {:a ref3
                    :b ref2}
@@ -69,6 +74,75 @@
         (fs/unify reg-vp
                   {:b {:root lavorare}})]
     ;; TODO: more tests.
-    (is (= (fs/get-in unified '(:a :italian)) "avere"))))
+    (is (= (fs/get-in unified '(:a :italian)) "avere"))
+    (printfs (list reg-vp {:b {:root lavorare}} unified) "lavorare.html")))
 
+
+(deftest t3
+  (let [rules
+        (let [passato-regular
+              (let [ref1 (ref :top)
+                    ref2 (ref :top)]
+                {:italian {:passato-prossimo-fn ref1}
+                 :infl :passato-prossimo
+                 :passato-prossimo-aux ref2
+                 :a {:italian ref1
+                     :passato-prossimo-aux ref2
+                     :infl :infinitive}})
+              passato-irregular
+              (let [ref1 (ref :top)
+                    ref2 (ref :top)]
+                {:italian ref1
+                 :infl :passato-prossimo
+                 :passato-prossimo-aux ref2
+                 :a {:passato-prossimo ref1
+                     :passato-prossimo-aux ref2
+                     :infl :infinitive}})
+              vp
+              (let [ref1 (ref :top)
+                    ref2 (ref :top)
+                    ref3 (ref :top)]
+                {:italian {:a ref1
+                           :b ref2}
+                 :infl ref3
+                 :a {:italian ref1
+                     :infl ref3}
+                 :b {:italian ref2}})]
+          (list passato-regular passato-irregular vp)) ;; end of rules
+        lexicon (let [fare {:infl :infinitive
+                            :italian "fare"
+                            :passato-prossimo-aux {:infl :infinitive
+                                                   :italian "avere"}}]
+                  (list
+                   {:infl :infinitive
+                    :italian "avere"}
+                   fare
+                   (let [ref (ref {:italian "fatto"})]
+                     {:italian ref
+                      :root (fs/merge
+                             fare
+                             {:passato-prossimo ref})})
+                   {:infl :infinitive
+                    :italian "lavorare"
+                    :passato-prossimo-aux {:infl :infinitive
+                                           :italian "avere"}}))]  ;; end of lexicon
+    ;; 1. choose rule at random.
+    (let [rule (nth rules (rand-int (.size rules)))
+          search-a (seq (search/query-with-lexicon (set lexicon) (list (fs/get-in rule '(:a)))))
+          search-b (seq (search/query-with-lexicon (set lexicon) (list (fs/get-in rule '(:b :root)))))
+          random-lexeme-a (if search-a (nth search-a (rand-int (.size search-a))))
+          random-lexeme-b (if search-b (nth search-b (rand-int (.size search-b))))]
+      (printfs
+       rules
+       "rules.html")
+      (printfs
+       lexicon
+       "lexicon.html")
+      (printfs
+       (list rule random-lexeme-a random-lexeme-b)
+       "random.html"))))
+
+
+          
+              
         
