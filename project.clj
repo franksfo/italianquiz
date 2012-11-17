8c752c0a142b8af1698a56d186be1ecd28f4c3a6
Eugene Joseph Koontz <ekoontz@apache.org>
2012-11-17 12:00:56 -0800

    first successful ring handler

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject italianverbs "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :plugins [[lein-ring "0.7.5"]]
+  :description "Italian language learning app"
+  :plugins [[lein-ring "0.7.3"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]
                  [congomongo "0.2.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [hiccup "1.0.1"]]
-  :dev-dependencies [[org.clojure/tools.nrepl "0.2.0-beta10"]]
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]
+                        [org.clojure/tools.nrepl "0.2.0-beta10"]]}}
   :ring {:handler italianverbs.core/app})
 
 
