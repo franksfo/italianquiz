f830b0ee9549c7f6cdd34600049b38481f9b2fbe
Eugene Koontz <ekoontz@hiro-tan.org>
2013-01-13 12:14:28 -0800

    whitespace

--- src/italianverbs/sandbox.clj
+++ src/italianverbs/sandbox.clj
@@ -3,9 +3,9 @@
    [clojure.core :exclude [find]]
    [italianverbs.lexiconfn]
    [italianverbs.lexicon]
-   [italianverbs.morphology]
    [italianverbs.generate]
    [italianverbs.grammar]
+   [italianverbs.morphology]
    [clojail.core :only [sandbox]]
    [clojail.testers]
    ]
@@ -15,21 +15,31 @@
    [clojure.string :as string]
    [clojure.tools.logging :as log]])
 
-(def workbook-sandbox (sandbox
+;; Sandbox specification derived from:
+;;   https://github.com/flatland/clojail/blob/4d3f58f69c2d22f0df9f0b843c7dea0c6a0a5cd1/src/clojail/testers.clj#L76
+;; and:
+;;    http://docs.oracle.com/javase/6/docs/api/overview-summary.html
+;; and:
+;;   http://richhickey.github.com/clojure/api-index.html
 
-                       (conj
-                        clojail.testers/secure-tester-without-def
-                        ;; taken from: http://docs.oracle.com/javase/6/docs/api/overview-summary.html
-                        ;; http://richhickey.github.com/clojure/api-index.html#
-                        (blacklist-nses '[clojure
-                                          java
-                                          javax
-                                          org.omg
-                                          org.w3c
-                                          org.xml
-                                          ]))
-                       :timeout 5000
-                       :namespace 'italianverbs.sandbox))
+(def workbook-sandbox
+  (sandbox
+   (conj
+    clojail.testers/secure-tester-without-def
+    (blacklist-nses '[clojure.main
+                      java
+                      javax
+                      org.omg
+                      org.w3c
+                      org.xml
+                      ])
+    (blacklist-objects [clojure.lang.Compiler
+                        clojure.lang.Ref
+                        clojure.lang.Reflector
+                        clojure.lang.Namespace
+                        clojure.lang.Var clojure.lang.RT]))
+   :timeout 5000
+   :namespace 'italianverbs.sandbox))
 
 (defn sandbox-load-string [expression]
   (workbook-sandbox (read-string expression)))
