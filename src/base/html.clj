1962e9c509543d9971811c21a83a12990d6c6c19
Eugene Koontz <ekoontz@hiro-tan.org>
2013-07-14 13:48:51 -0700

    hide 'i tuoi preferiti' again; not a priority to implement at this time

--- src/base/html.clj
+++ src/base/html.clj
@@ -41,9 +41,9 @@
     [:div
      (if (= relative-url "/workbook/") {:class "selected"})
      [:a {:href "/italian/workbook/"} "Libro di Lavoro"  ] ]
-    [:div
-     (if (= relative-url "/preferiti/") {:class "selected"})
-     [:a {:href "/italian/preferiti/"} "I tuoi preferiti"]]
+;    [:div
+;     (if (= relative-url "/preferiti/") {:class "selected"})
+;     [:a {:href "/italian/preferiti/"} "I tuoi preferiti"]]
     [:div
      (if (= relative-url "/about/") {:class "selected"})
      [:a {:href "/italian/about/"} "Che è?"  ] ]
