78259315b9305aacd6166e7ce9ab96d325968dab
Eugene Koontz <ekoontz@hiro-tan.org>
2013-07-14 22:03:25 -0700

    add {:synsem {:essere true}} to essere-common

--- src/italianverbs/lexicon.clj
+++ src/italianverbs/lexicon.clj
@@ -1243,7 +1243,8 @@
 
 (def essere-common
   (unify
-   {:italian {:infinitive "essere"
+   {:synsem {:essere true}
+    :italian {:infinitive "essere"
               :essere true
               :irregular {:present {:1sing "sono"
                                     :2sing "sei"
