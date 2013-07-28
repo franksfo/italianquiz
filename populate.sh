0d4e90d9ddb186b5d6762d0b93f8bea95e9d9f9d
Eugene Koontz <ekoontz@hiro-tan.org>
2013-07-28 13:21:38 -0700

    add comments about usage.

--- populate.sh
+++ populate.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
+# usage:  populate.sh [count]
+#  where _count_ is how many sentences to generate.
+#   defaults to 100 (see src/italianverbs/populate.clj for 
+#   where default is set).
 lein run -m italianverbs.populate $*
