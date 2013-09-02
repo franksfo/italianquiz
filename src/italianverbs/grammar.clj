72b8363d1e865299fca0e5641653ee3da8b3229e
Eugene Koontz <ekoontz@hiro-tan.org>
2013-09-01 20:24:29 -0700

    attempt to use hh21 causes (for now):
    ClassCastException clojure.lang.PersistentArrayMap cannot be cast to java.lang.CharSequence  clojure.core/re-matcher (core.clj:4386)

--- src/italianverbs/grammar.clj
+++ src/italianverbs/grammar.clj
@@ -1102,21 +1102,16 @@
 
 (defn take-gen4-random [n]
   (take n
-        (gen/gen14 (list cc10)
-                   (fn []
-                     (gen/gen14 (list ch21)
-                                (fn []
-                                  (shuffle lex/tinylex))
-                                (fn []
-                                  (shuffle lex/tinylex))
-                                sent-impl 0))
+        (gen/gen14 (list hh21)
+                   (fn [] (shuffle lex/tinylex))
                    (fn []
                      (gen/gen14 (list cc10)
-                                (fn [] (shuffle lex/tinylex))
-                                (fn [] (shuffle lex/tinylex))
+                                (fn []
+                                  (shuffle lex/tinylex))
+                                (fn []
+                                  (shuffle lex/tinylex))
                                 sent-impl 0))
                    sent-impl 0)))
-
 
 ;; TODO: move to somewhere else that uses both grammar and lexicon (e.g. quiz or workbook): grammar itself should not depend on lexicon (lex/lexicon).
 (defn random-sentence []
