b09280ba2a667cc433c53d27eec84840bc3dbca0
Eugene Koontz <ekoontz@hiro-tan.org>
2013-10-29 08:32:37 -0700

    working on past modal conjugations in english (e.g. 'wanted to sleep')

---
+++ src/italianverbs/past-modal.clj
@@ -0,0 +1,16 @@
+(ns italianverbs.past-modal
+  (:refer-clojure :exclude [get-in merge resolve find])
+  (:require
+   [italianverbs.workbook :refer :all]
+   [clojure.core :as core]
+   [italianverbs.generate :refer :all]
+   [italianverbs.grammar :refer :all]
+   [italianverbs.lexicon :refer :all]
+   [italianverbs.lexiconfn :refer :all]
+   [italianverbs.morphology :refer [finalize fo]]
+   [italianverbs.rules :refer :all]
+   [italianverbs.ug :refer :all]
+   [italianverbs.unify :refer :all :exclude [unify]]))
+
+(def want-to-sleep (take 1 (over modal-vp {:synsem {:sem {:obj {:pred :dormire} :pred :volere}}})))
+
