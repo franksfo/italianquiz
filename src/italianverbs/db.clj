c2585ae3c4bd0e3c8661f7536c4edcb6fa6581da
Eugene Koontz <ekoontz@hiro-tan.org>
2014-02-10 09:10:01 -0800

    add sqlite :require

--- src/italianverbs/db.clj
+++ src/italianverbs/db.clj
@@ -2,7 +2,8 @@
 ;; whose session is 'y' where 'x' != 'y'.
 ;; (see update-question-by-id-with-guess) where this is enforced by the db/fetch's :where clause.
 (ns italianverbs.db
-  (:require [somnium.congomongo :as mongo] ;; TODO: provide database abstraction over mongo and other possible backing stores.
+  (:require [somnium.congomongo :as mongo]
+            [clojure.java.jdbc :as jdbc]
             ))
 
 (defn fetch [ & args]
