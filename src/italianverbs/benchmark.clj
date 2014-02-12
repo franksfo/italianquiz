c0b05cface70084ca6710f44fc3d80f5cacc71fa
Eugene Koontz <ekoontz@hiro-tan.org>
2014-02-11 23:24:50 -0800

    (def) the (time ..) in order to get (load) to run the benchmark

--- src/italianverbs/benchmark.clj
+++ src/italianverbs/benchmark.clj
@@ -2,6 +2,6 @@
   (:require
    [italianverbs.grammar :refer (grammar)]
    [italianverbs.morphology :refer (fo)]
-   [italianverbs.over :refer :all])
+   [italianverbs.over :refer :all]))
 
-  (time (fo (take 1 (over grammar "io" (over grammar "essere" (over grammar "andare" (over grammar "a" (over grammar "il" "mercato")))))))))
+(def results (time (fo (take 1 (over grammar "io" (over grammar "essere" (over grammar "andare" (over grammar "a" (over grammar "il" "mercato")))))))))
