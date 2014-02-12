547d0ef6287a6c5786bde776ace0feba6d24b187
Eugene Koontz <ekoontz@hiro-tan.org>
2014-02-11 23:34:16 -0800

    make benchmarks easier to run

--- src/italianverbs/benchmark.clj
+++ src/italianverbs/benchmark.clj
@@ -4,4 +4,26 @@
    [italianverbs.morphology :refer (fo)]
    [italianverbs.over :refer :all]))
 
-(def results (time (fo (take 1 (over grammar "io" (over grammar "essere" (over grammar "andare" (over grammar "a" (over grammar "il" "mercato")))))))))
+(def results-small-fn (fn [] (time (fo (take 1 (over grammar 
+                                                     "io" 
+                                                     "dormire"))))))
+
+(defn run-small [n]
+  (take n (repeatedly #(results-small-fn))))
+
+(def results-medium-fn (fn [] (time (fo (take 1 (over grammar 
+                                                      "io" 
+                                                      (over grammar 
+                                                            "essere" 
+                                                            (over grammar 
+                                                                  "andare" 
+                                                                  (over grammar 
+                                                                        "a" 
+                                                                        (over grammar 
+                                                                              "il" 
+                                                                              "mercato"))))))))))
+
+(defn run-medium [n]
+  (take n (repeatedly #(results-medium-fn))))
+
+
