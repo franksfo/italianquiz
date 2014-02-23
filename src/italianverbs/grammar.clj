5dca200e6e3868818891a11b9f74d70ef7aa5156
Eugene Koontz <ekoontz@hiro-tan.org>
2014-02-23 15:28:47 -0800

    use :rule rather than :comment - leaving :comment for schema (italianverb/ug); whereas :comment will be for language specific grammatical rules

--- src/italianverbs/grammar.clj
+++ src/italianverbs/grammar.clj
@@ -14,6 +14,123 @@
 
 ;; TODO: s/unifyc/unify/
 
+(def grammar (list (unifyc hh21
+                           {:rule "adjective-phrase"
+                            :synsem {:cat :adjective}})
+
+                   (unifyc hh21
+                           (let [head-synsem {:cat :intensifier
+                                              :modified true}]
+                             {:rule "intensifier-phrase"
+                              :synsem head-synsem}))
+
+                   (unifyc hc11-comp-subcat-1
+                           (let [head-synsem {:cat :noun
+                                              :modified true}]
+                             {:comp {:phrasal false ;; rathole prevention
+                                     :synsem {:cat :adjective
+                                              :mod head-synsem}}
+                              :head {:synsem {:modified false}}
+                              :rule "nbar"
+                              :synsem head-synsem}))
+
+                   (unifyc cc10
+                           {:rule "noun-phrase"
+                            :synsem {:cat :noun}
+                            :comp {:phrasal false}}) ;; rathole prevention
+
+                   (unifyc hh10
+                           {:rule "prepositional-phrase"
+                            :synsem {:cat :prep}})
+
+                   (unifyc cc10
+                           {:head {:synsem {:aux true}}
+                            :rule "s-aux"
+                            :synsem {:infl :present
+                                     :cat :verb
+                                     :sem {:tense :past}}})
+
+                   (unifyc cc10
+                          {:rule "s-future"
+                           :synsem {:aux false
+                                    :infl :futuro
+                                    :cat :verb
+                                    :sem {:tense :future}}})
+
+
+                   (unifyc cc10
+                           {:rule "s-imperfetto"
+                            :synsem {:aux false
+                                     :infl :imperfetto
+                                     :cat :verb
+                                     :sem {:tense :past}}})
+
+                   (unifyc cc10
+                           {:rule "s-present"
+                            :synsem {:aux false
+                                     :infl :present
+                                     :cat :verb
+                                     :sem {:tense :present}}})
+
+                   (unifyc hh21
+                           {:rule "vp-infinitive"
+                            :synsem {:aux false
+                                     :infl :infinitive
+                                     :cat :verb}})
+
+                   (unifyc hh21
+                           {:rule "vp-aux"
+                            :synsem {:aux true
+                                     :infl :present
+                                     :sem {:tense :past}
+                                     :cat :verb}})
+
+                   ;; this rule is kind of complicated and made more so by
+                   ;; dependence on auxilary sense of "avere" which supplies the
+                   ;; obj-agr agreement between the object and the main (non-auxilary) verb.
+                   (unifyc hh22
+                           (let [obj-agr (ref :top)]
+                             {:rule "vp-aux-22"
+                              :synsem {:aux true
+                                       :infl :present
+                                       :sem {:tense :past}
+                                       :subcat {:2 {:agr obj-agr}}
+                                       :cat :verb}
+                              :italian {:b {:obj-agr obj-agr}}}))
+
+                   (unifyc hh21
+                           {:rule "vp-future"
+                            :synsem {:aux false
+                                     :infl :futuro
+                                     :cat :verb}})
+
+                   (unifyc hh21
+                          {:rule "vp-imperfetto"
+                           :synsem {:aux false
+                                    :infl :imperfetto
+                                    :cat :verb}})
+
+                   (unifyc hh21
+                           {:rule "vp-past"
+                            :synsem {:aux false
+                                     :infl :past
+                                     :cat :verb}})
+
+                   (unifyc hh21
+                           {:rule "vp-present"
+                            :synsem {:aux false
+                                     :infl :present
+                                     :sem {:tense :present}
+                                     :cat :verb}})
+
+                   (unifyc ch21
+                           {:comp {:synsem {:cat :noun
+                                            :pronoun true}}
+                            :rule "vp-pronoun"
+                            :synsem {:cat :verb
+                                     :infl {:not :past}}})))
+
+
 (defn aux-is-head-feature [phrase]
   (cond (= :verb (get-in phrase '(:synsem :cat)))
         (unifyc phrase
@@ -21,123 +138,6 @@
                   {:synsem {:aux ref}
                    :head {:synsem {:aux ref}}}))
         true phrase))
-
-(def grammar (list (merge (unifyc hh21
-                                  {:synsem {:cat :adjective}})
-                          {:comment "adjective-phrase"})
-                                           
-                   (merge (unifyc hh21
-                                  (let [head-synsem {:cat :intensifier
-                                                     :modified true}]
-                                    {:synsem head-synsem}))
-                          {:comment "intensifier-phrase"})
-
-                   (merge (unifyc hc11-comp-subcat-1
-                                  (let [head-synsem {:cat :noun
-                                                     :modified true}]
-                                    {:synsem head-synsem
-                                     :head {:synsem {:modified false}}
-                                     :comp {:phrasal false ;; rathole prevention
-                                            :synsem {:cat :adjective
-                                                     :mod head-synsem}}}))
-                          {:comment "nbar"})
-
-                   (merge (unifyc cc10
-                                  {:synsem {:cat :noun}
-                                   :comp {:phrasal false}}) ;; rathole prevention
-                          {:comment "noun-phrase"})
-
-                   (merge (unifyc hh10
-                                  {:synsem {:cat :prep}})
-                          {:comment "prepositional-phrase"})
-
-                   (merge (unifyc cc10
-                                  {:head {:synsem {:aux true}}
-                                   :synsem {:infl :present
-                                            :cat :verb
-                                            :sem {:tense :past}}})
-                          {:comment "s-aux"})
-
-                   (merge (unifyc cc10
-                                  {:synsem {:aux false
-                                            :infl :futuro
-                                            :cat :verb
-                                            :sem {:tense :future}}})
-                          {:comment "s-future"})
-
-                   (merge (unifyc cc10
-                                  {:synsem {:aux false
-                                            :infl :imperfetto
-                                            :cat :verb
-                                            :sem {:tense :past}}})
-                          {:comment "s-imperfetto"})
-                   
-                   (merge (unifyc cc10
-                                  {:synsem {:aux false
-                                            :infl :present
-                                            :cat :verb
-                                            :sem {:tense :present}}})
-                          {:comment "s-present"})
-
-                   (merge (unifyc hh21
-                                  {:synsem {:aux false
-                                            :infl :infinitive
-                                            :cat :verb}})
-                          {:comment "vp-infinitive"})
-
-                   (merge (unifyc hh21
-                                  {:synsem {:aux true
-                                            :infl :present
-                                            :sem {:tense :past}
-                                            :cat :verb}})
-                          {:comment "vp-aux"})
-
-                   ;; this rule is kind of complicated and made more so by
-                   ;; dependence on auxilary sense of "avere" which supplies the
-                   ;; obj-agr agreement between the object and the main (non-auxilary) verb.
-                   (merge (unifyc hh22
-                                  (let [obj-agr (ref :top)]
-                                    {:synsem {:aux true
-                                              :infl :present
-                                              :sem {:tense :past}
-                                              :subcat {:2 {:agr obj-agr}}
-                                              :cat :verb}
-                                     :italian {:b {:obj-agr obj-agr}}}))
-                          {:comment "vp-aux-22"})
-
-                   (merge (unifyc hh21
-                                  {:synsem {:aux false
-                                            :infl :futuro
-                                            :cat :verb}})
-                          {:comment "vp-future"})
-
-                   (merge (unifyc hh21
-                                  {:synsem {:aux false
-                                            :infl :imperfetto
-                                            :cat :verb}})
-                          {:comment "vp-imperfetto"})
-
-                   (merge (unifyc hh21
-                                  {:synsem {:aux false
-                                            :infl :past
-                                            :cat :verb}})
-                          {:comment "vp-past"})
-
-                   (merge (unifyc hh21
-                                  {:synsem {:aux false
-                                            :infl :present
-                                            :sem {:tense :present}
-                                            :cat :verb}})
-                          {:comment "vp-present"})
-
-                   (merge (unifyc ch21
-                                  {:comp {:synsem {:cat :noun
-                                                   :pronoun true}}
-                                   :synsem {:cat :verb
-                                            :infl {:not :past}}})
-                          {:comment "vp-pronoun"})
-
-))
 
 (def grammar
   (map (fn [phrase]
@@ -149,6 +149,6 @@
 ;; TODO: calling (.size) because (map) is lazy, and I want to realize
 ;; the sequence - must be a better way to loop over the grammar and realize the result.
 (.size (map (fn [rule]
-       (intern *ns* (symbol (:comment rule)) rule))
+       (intern *ns* (symbol (:rule rule)) rule))
      grammar))
 
