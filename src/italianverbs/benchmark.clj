579fa75687a559fd464034f4287ae02ad5d95422
Eugene Koontz <ekoontz@hiro-tan.org>
2014-04-20 18:37:11 -0700

    add hpcl test

--- src/italianverbs/benchmark.clj
+++ src/italianverbs/benchmark.clj
@@ -281,6 +281,11 @@
    #(fo-ps (first (take 1 (forest/hp cache grammar {:synsem {:subcat '()}}))))
    trials))
 
+(defn run-hpcl-with-subcat-nil-test [trials]
+  (run-benchmark
+   #(fo-ps (first (take 1 (forest/hpcl cache grammar {:synsem {:subcat '()}}))))
+   trials))
+
 (defn run-hlcl-test-aux [trials]
 "this one seems to be an outlier for hlcl - try to make it faster."
 (let [essere true
@@ -320,7 +325,12 @@
 (defn benchmark []
   (println "run-hlcl-test 10")
   (run-hlcl-test 10)
+
   (println "run-hp-with-subcat-nil-test 10")
   (run-hp-with-subcat-nil-test 10)
+
+  (println "run-hpcl-with-subcat-nil-test 10")
+  (run-hpcl-with-subcat-nil-test 10)
+
   (println "run-hlcl-test-aux 10")
   (run-hlcl-test-aux 10))
