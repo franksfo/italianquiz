cdc8894b61fc0197c29500c4b0393cddf6de7760
Eugene Koontz <ekoontz@hiro-tan.org>
2014-04-20 17:07:04 -0700

    update benchmark.sh to use latest benchmark formatting

--- benchmark.sh
+++ benchmark.sh
@@ -2,4 +2,4 @@
 # usage:  benchmark.sh
 # show only benchmark name and stats: ./benchmark.sh | egrep "\-\-"\|average\|stddev
 git log -1
-lein run -m italianverbs.benchmark/benchmark $* | egrep "\-\-"\|average\|stddev
+lein run -m italianverbs.benchmark/benchmark $*
