d55cacab14c5e5c5ac992308ab6e469ff3829b90
Eugene Koontz <ekoontz@hiro-tan.org>
2014-05-18 16:23:36 -0700

    set up default id and sequence for tags (vgroups)

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -1,3 +1,7 @@
+-- Must do this with DB superuser privileges:
+--
+-- CREATE DATABASE verbcoach OWNER verbcoach;
+-- 
 --
 -- PostgreSQL database dump
 --
@@ -13,14 +17,14 @@ SET client_min_messages = warning;
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+-- CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+-- COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 SET search_path = public, pg_catalog;
@@ -44,32 +48,11 @@ CREATE TABLE verb (
 ALTER TABLE public.verb OWNER TO verbcoach;
 
 --
--- Name: verb_id_seq; Type: SEQUENCE; Schema: public; Owner: verbcoach
---
-
-CREATE SEQUENCE verb_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.verb_id_seq OWNER TO verbcoach;
-
---
--- Name: verb_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: verbcoach
---
-
-ALTER SEQUENCE verb_id_seq OWNED BY verb.id;
-
-
---
 -- Name: vgroup; Type: TABLE; Schema: public; Owner: verbcoach; Tablespace: 
 --
 
 CREATE TABLE vgroup (
-    id integer NOT NULL,
+    id bigint NOT NULL,
     created timestamp without time zone DEFAULT now(),
     updated timestamp without time zone,
     name text
@@ -82,34 +65,30 @@ ALTER TABLE public.vgroup OWNER TO verbcoach;
 -- Name: id; Type: DEFAULT; Schema: public; Owner: verbcoach
 --
 
+-- Name: verb_id_seq; Type: SEQUENCE; Schema: public; Owner: verbcoach
+--
+
+CREATE SEQUENCE verb_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE vgroup_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE public.verb_id_seq OWNER TO verbcoach;
+
 ALTER TABLE ONLY verb ALTER COLUMN id SET DEFAULT nextval('verb_id_seq'::regclass);
 
+ALTER TABLE public.vgroup_id_seq OWNER TO verbcoach;
 
---
--- Data for Name: verb; Type: TABLE DATA; Schema: public; Owner: verbcoach
---
-
-COPY verb (id, created, updated, value) FROM stdin;
-8	2014-05-17 20:29:51.006055	\N	{:italian "parlare"}
-12	2014-05-18 10:37:42.228481	\N	{:italian "dormire"}
-17	2014-05-18 13:37:32.398871	\N	{:italian "dire"}
-\.
-
-
---
--- Name: verb_id_seq; Type: SEQUENCE SET; Schema: public; Owner: verbcoach
---
-
-SELECT pg_catalog.setval('verb_id_seq', 17, true);
-
-
---
--- Data for Name: vgroup; Type: TABLE DATA; Schema: public; Owner: verbcoach
---
-
-COPY vgroup (id, created, updated, name) FROM stdin;
-\.
-
+ALTER TABLE ONLY vgroup ALTER COLUMN id SET DEFAULT nextval('vgroup_id_seq'::regclass);
 
 --
 -- Name: verb_pkey; Type: CONSTRAINT; Schema: public; Owner: verbcoach; Tablespace: 
@@ -118,7 +97,6 @@ COPY vgroup (id, created, updated, name) FROM stdin;
 ALTER TABLE ONLY verb
     ADD CONSTRAINT verb_pkey PRIMARY KEY (id);
 
-
 --
 -- Name: vgroup_pkey; Type: CONSTRAINT; Schema: public; Owner: verbcoach; Tablespace: 
 --
@@ -126,17 +104,17 @@ ALTER TABLE ONLY verb
 ALTER TABLE ONLY vgroup
     ADD CONSTRAINT vgroup_pkey PRIMARY KEY (id);
 
+--
+-- Name: verb_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: verbcoach
+--
+
+ALTER SEQUENCE verb_id_seq OWNED BY verb.id;
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: ekoontz
+-- Name: vgroup_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: verbcoach
 --
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM ekoontz;
-GRANT ALL ON SCHEMA public TO ekoontz;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
+ALTER SEQUENCE vgroup_id_seq OWNED BY vgroup.id;
 --
 -- PostgreSQL database dump complete
 --
--- src/sql/permissions.sql
+++
@@ -1,6 +0,0 @@
--- Run as psql superuser
---
-GRANT ALL ON TABLE vgroup TO verbcoach;
-GRANT ALL ON SEQUENCE verb_id_seq TO verbcoach;
-ALTER TABLE verb OWNER TO verbcoach;
-ALTER TABLE vgroup OWNER TO verbcoach;
