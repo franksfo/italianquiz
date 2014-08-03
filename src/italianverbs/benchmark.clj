0a548833e7022f621135d98447c1a8d9104bd5c5
Eugene Koontz <ekoontz@hiro-tan.org>
2014-08-03 00:33:28 -0700

    -cleanup -make (standard-benchmark) generate for both italian and english

--- src/italianverbs/benchmark.clj
+++ src/italianverbs/benchmark.clj
@@ -8,8 +8,8 @@
    [clojure.tools.logging :as log]
    [italianverbs.forest :as forest] ;; this allows us to use newly-defined functions from the forest namespace.
    [italianverbs.generate :refer :all]
-   [italianverbs.grammar.english :as english]
-   [italianverbs.grammar.italiano :as italiano]
+   [italianverbs.grammar.english :as en]
+   [italianverbs.grammar.italiano :as it]
    [italianverbs.lexicon :refer :all]
    [italianverbs.morphology :refer (fo fo-ps)]
    [italianverbs.over :refer (overc overh)]
@@ -99,8 +99,8 @@
    #(fo (generate {:comp {:phrasal false}
                    :head {:phrasal false}}
                   lexicon
-                  (list italiano/s-present)
-                  italiano/cache))
+                  (list it/s-present)
+                  it/cache))
    trials
    "sentence which is simply a subject plus a verb"))
 
@@ -109,96 +109,15 @@
    #(fo (generate
          {:synsem {:subcat '()}}
          lexicon
-         (list italiano/s-aux italiano/vp-aux) italiano/cache))
+         (list it/s-aux it/vp-aux) it/cache))
    trials
    "saux"))
 
-(defn run-hlcp2 [trials]
+(defn run-sentence [trials]
   (run-benchmark
    #(fo (sentence {:synsem {:sem {:pred :impazzire}}}))
    trials
    "sentence with 'impazzire'"))
-
-(defn run-hlcl [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/hlcl italiano/cache italiano/grammar {:synsem {:cat :verb :subcat '()}}))))
-   trials
-   "hlcl"))
-
-(defn run-hlcp [trials]
-  (run-benchmark
-   #(fo-ps (first (take 1 (forest/hlcp italiano/cache italiano/grammar {:synsem {:cat :verb :subcat '()}}))))
-   trials
-   "hlcp with empty subcat"))
-
-(defn run-hpcl [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/hpcl italiano/cache italiano/grammar {:synsem {:cat :verb :subcat '()}}))))
-   trials
-   "hpcl"))
-
-(defn run-hpcl-v [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/hpcl italiano/cache italiano/grammar {:synsem {:cat :verb
-                                                            :infl :futuro
-                                                            :sem {:obj {:pred :ridere} 
-                                                                  :pred :volere
-;                                                                  :tense :futuro
-                                                                  :subj {:pred :paola}}
-                                                            :subcat '()}}))))
-   trials
-   "hpcl with empty subcat and pred=volere"))
-
-(defn run-hpcp [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/hpcp italiano/cache italiano/grammar {:synsem {:cat :verb :subcat '()}}))))
-   trials
-   "hpcp with empty subcat"))
-
-(defn run-test [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/hlcl italiano/cache (list italiano/vp-aux) {:synsem {:cat :verb 
-                                                                  :subcat {:1 :top}}
-                                                         :head {:subcat {:1 :top
-                                                                         :2 :top}
-                                                                :aux true}}))))
-   trials
-   "vp-aux"))
-
-(defn run-hlcl-test [trials]
-  (run-benchmark
-   #(fo-ps (first (take 1 (forest/hlcl italiano/cache italiano/grammar :top))))
-   trials
-   "hlcl"))
-
-(defn run-hlcl-with-subcat-nil-test [trials]
-  (run-benchmark
-   #(fo-ps (first (take 1 (forest/hlcl italiano/cache italiano/grammar {:synsem {:subcat '()}}))))
-   trials
-   "hlcl with empty subcat"))
-
-(defn run-hpcl-with-subcat-nil-test [trials]
-  (run-benchmark
-   #(fo-ps (first (take 1 (forest/hpcl italiano/cache italiano/grammar {:synsem {:subcat '()}}))))
-   trials
-   "hpcl with empty subcat"))
-
-(defn run-hpcp-with-subcat-nil-test [trials]
-  (run-benchmark
-   #(fo-ps (first (take 1 (forest/hpcp italiano/cache italiano/grammar {:synsem {:subcat '()}}))))
-   trials
-   "hpcp with empty subcat"))
-
-(defn run-suoceri [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/hpcp italiano/cache italiano/grammar {:synsem {:cat :verb
-                                                            :aux false
-                                                            :infl :imperfetto
-                                                            :sem {:pred :amare
-                                                                  :obj {:pred :giorgio}
-                                                                  :subj {:pred :suoceri}}
-                                                            :subcat '()}}))))
-   trials))
 
 (def catspec-s
   {:synsem {:cat :verb
@@ -212,13 +131,13 @@
   (filter (fn [rule]
             (not (fail? rule)))
           (map #(unifyc % catspec-s)
-               italiano/grammar)))
+               it/grammar)))
 
 (def grammar-0
   (filter (fn [rule]
             (not (fail? rule)))
           (map #(unifyc % {:synsem {:subcat '()}})
-               italiano/grammar)))
+               it/grammar)))
 
 (def grammar-1
   (filter (fn [rule]
@@ -226,7 +145,7 @@
           (mapcat (fn [grammar-0-rule]
                     (map (fn [grammar-rule]
                            (unifyc grammar-0-rule {:head grammar-rule}))
-                         italiano/grammar))
+                         it/grammar))
                   grammar-0)))
 
 (def grammar-2
@@ -235,7 +154,7 @@
           (mapcat (fn [grammar-1-rule]
                     (map (fn [grammar-rule]
                            (unifyc grammar-1-rule {:head {:head grammar-rule}}))
-                         italiano/grammar))
+                         it/grammar))
                   grammar-1)))
 
 (def grammar-3
@@ -244,7 +163,7 @@
           (mapcat (fn [grammar-2-rule]
                     (map (fn [grammar-rule]
                            (unifyc grammar-2-rule {:head {:head {:head grammar-rule}}}))
-                         italiano/grammar))
+                         it/grammar))
                   grammar-2)))
 
 (def grammar-full
@@ -259,17 +178,17 @@
                            (unifyc grammar-rule catspec-grammar-0-rule-head))
                          (map #(unify/get-in % [:head])
                               catspec-grammar-0)))
-                  italiano/grammar)))
+                  it/grammar)))
 
 ;; (type cp-over-hl) => lazyseq
 ;; (fo-ps (take 1 (cp-over-hl hl-over-cg1h)))
 ;; "[vp-imperfetto amare (were loving) [noun-phrase il vostro (your (pl) ) gatto (cat)]]"
 (defn catlove []
-  (forest/hlcp italiano/cache italiano/grammar {:synsem {:cat :verb
-                                       :aux false
-                                       :infl :imperfetto
-                                       :sem {:pred :amare
-                                             :obj {:pred :gatto}}}}))
+  (forest/gen2 it/cache it/grammar {:synsem {:cat :verb
+                                             :aux false
+                                             :infl :imperfetto
+                                             :sem {:pred :amare
+                                                   :obj {:pred :gatto}}}}))
 
 (defn run-gatto [trials]
   (run-benchmark
@@ -281,64 +200,30 @@
 ;; will work with inanimate subjects (only exception currently is ‘essere’ (e.g. ‘la cipolle è’ ..)
 ;;
 ;; To reproduce problem, use: {:sem {:subj {:animate false}}}
-(defn bolt-benchmark [trials grammar cache]
+(defn bolt-benchmark [trials grammar cache name]
   (let [spec {:synsem {:cat :verb
                        :aux false
                        :sem {:subj {:animate true}}
                        :subcat '()}}]
     (run-benchmark #(fo (first (take 1 (forest/gen2 grammar lexicon spec cache))))
                    trials
-                   "standard-bolt-benchmark")))
+                   name)))
 
 (defn standard-benchmark-it [ & [ trials ]]
   (let [trials
         (if (nil? trials) 1 trials)]
-    (bolt-benchmark trials italiano/grammar italiano/cache)))
-
-(defn standard-benchmark [ & [ trials ]]
-  (let [trials
-        (if (nil? trials) 1 trials)]
-    (standard-benchmark-it trials)))
+    (bolt-benchmark trials it/grammar it/cache "bolt-benchmark-it")))
 
 (defn standard-benchmark-en [ & [ trials ]]
   (let [trials
         (if (nil? trials) 1 trials)]
-    (bolt-benchmark trials english/grammar english/cache)))
+    (bolt-benchmark trials en/grammar en/cache "bolt-benchmark-en")))
 
-(defn run-hlcp-with-subcat-nil-test [trials]
-  (run-benchmark
-   #(fo-ps (first (take 1 (forest/hlcp italiano/cache italiano/grammar {:synsem {:subcat '()}}))))
-   trials
-   "hlcp with empty subcat"))
-
-(defn run-hlcl-test-aux [trials]
-  ;; ?? this comment says hlcl: TODO resolve if this is also an outlier for its respective generic
-  ;; generation rule (hl).
-  "this one seems to be an outlier for hlcl - try to make it faster."
-  (let [essere true
-        grammar italiano/grammar]
-    (run-benchmark
-   
-     #(fo (first (take 1 (forest/hlcl italiano/cache grammar {:synsem {:sem {:pred :venire}
-                                                                       :essere essere
-                                                                       :aux true}}))))
-     trials
-     "hlcl with aux = true and pred=venire")))
-
-(defn run-hpcp2 [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/hpcp italiano/cache italiano/grammar lexicon {:synsem {:infl :futuro :cat :verb :subcat '()}
-                                                           :head {:synsem {:subcat {:1 :top
-                                                                                    :2 '()}}}}))))
-   trials))
-
-(defn run-hxcx [trials]
-  (run-benchmark
-   #(fo (first (take 1 (forest/gen2 italiano/cache italiano/grammar lexicon
-                                    {:synsem {:infl :futuro :cat :verb :subcat '()}
-                                     :head {:synsem {:subcat {:1 :top
-                                                              :2 '()}}}}))))
-   trials))
+(defn standard-benchmark [ & [ trials ]]
+  (let [trials
+        (if (nil? trials) 1 trials)]
+    (do (standard-benchmark-it trials)
+        (standard-benchmark-en trials))))
 
 (defn word-speaker [trials]
   (run-benchmark
@@ -346,14 +231,14 @@
                             :sem {:pred :parlare
                                   :subj {:pred :lei}
                                   :obj {:pred :parola}}}}
-                  lexicon italiano/grammar))
+                  lexicon it/grammar))
    trials))
 
 (defn word-spoken [trials]
   (run-benchmark
    #(fo-ps (generate {:synsem {:subcat '() :cat :noun
                                :sem {:pred :parola}}}
-                     lexicon italiano/grammar))
+                     lexicon it/grammar))
    trials))
 
 (defn word-spoken2 [trials]
@@ -361,33 +246,13 @@
     (run-benchmark
      #(fo-ps (generate {:synsem {:subcat '() :cat :noun
                                  :sem {:pred :parola}}}
-                       lexicon (list italiano/np italiano/nbar)))
+                       lexicon (list it/np it/nbar)))
      trials)))
 
 (defn word-speaker-lb [trials]
   (run-benchmark
-   #(fo (take 1 (forest/lightning-bolt italiano/grammar lexicon {:synsem {:subcat '() :cat :verb
-                                                                          :sem {:pred :parlare
-                                                                                :subj {:pred :lei}
-                                                                                :obj {:pred :parola}}}})))
+   #(fo (take 1 (forest/lightning-bolt it/grammar lexicon {:synsem {:subcat '() :cat :verb
+                                                                    :sem {:pred :parlare
+                                                                          :subj {:pred :lei}
+                                                                          :obj {:pred :parola}}}})))
    trials))
-
-(defn benchmark []
-  (println "run-hlcl-test 10")
-  (run-hlcl-test 10)
-
-  (println "run-hlcl-test-aux 10")
-  (run-hlcl-test-aux 10)
-
-  (println "run-hlcp-with-subcat-nil-test 10")
-  (run-hlcp-with-subcat-nil-test 10)
-
-  (println "run-hpcl-with-subcat-nil-test 10")
-  (run-hpcl-with-subcat-nil-test 10)
-
-  (println "run-hpcp-with-subcat-nil-test 10")
-  (run-hpcp-with-subcat-nil-test 10))
-
-
-
-
