dfe277880448c5c87730b8b3ffdddff5bae5ea9e
Eugene Koontz <ekoontz@hiro-tan.org>
2014-08-23 13:20:48 -0700

    more game tweaks

--- src/italianverbs/game.clj
+++ src/italianverbs/game.clj
@@ -7,11 +7,59 @@
 (defn game []
   (html5
    [:div#game
-    [:i {:class "fa fa-camera-retro fa-lg"} ]
-    [:i {:class "fa fa-camera-retro fa-2x"} ]
-    [:i {:class "fa fa-camera-retro fa-3x"} ]
-    [:i {:class "fa fa-camera-retro fa-4x"} ]
-    [:i {:class "fa fa-camera-retro fa-5x"} ]]))
+
+
+
+    [:div#rainforest
+     
+     [:div#sky
+
+      [:i {:class "fa fa-cloud fa-6x"
+           :style "left:130px; top:10px"}]
+      
+      [:i {:class "fa fa-cloud medium-cloud storm-cloud"
+           :style "left:230px; top:10px;"}]
+
+      [:i {:class "fa fa-cloud big-cloud"
+           :style "left:330px; top:30px;"}]
+      
+      [:i {:class "fa fa-cloud"
+           :style "left:450px; font-size:75px; top:15px;"}]
+      ]
+
+     [:div#ground
+
+      [:i {:class "fa fa-tree fa-5x"
+           :style "left:50px;top:-120px;"}
+       ]
+
+      [:div {:class "fa fa-tree"
+             :style "left:80px; top:-100px;font-size:160px"}]
+      
+      [:i {:class "fa fa-tree fa-5x"
+           :style "left:180px;top:-120px;"}
+       ]
+
+      [:i {:class "fa fa-tree fa-3x"
+           :style "left:220px;top:-120px;"}
+       ]
+
+      [:i {:class "fa fa-tree fa-4x"
+           :style "left:330px;top:-120px;"}
+       ]
+
+      [:div {:class "fa fa-tree"
+             :style "left:400px; top:-110px;font-size:140px"}]
+      
+
+
+      ]]
+] ; html5/div
+))
+
+
+
+
 
 
 
