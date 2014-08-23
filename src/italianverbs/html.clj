5b4dc345937f048d7c4fb1eef0b4b0d7367b4c6d
Eugene Koontz <ekoontz@hiro-tan.org>
2014-08-23 13:21:24 -0700

    add game css include

--- src/italianverbs/html.clj
+++ src/italianverbs/html.clj
@@ -692,6 +692,7 @@
    (h/include-css "/css/prettify.css")
    
    (include-css "http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css")
+   (include-css "/css/game.css")
 
    [:style {:type "text/css"} "ul { padding-left: 2em }"]
    
