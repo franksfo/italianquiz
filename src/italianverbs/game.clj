5b6279455069cd933df76aed2d5dfbdc568ebb12
Eugene Koontz <ekoontz@hiro-tan.org>
2014-09-01 01:03:35 -0700

    integrated SVG with rest of game layers more or less successfully

--- src/italianverbs/game.clj
+++ src/italianverbs/game.clj
@@ -19,46 +19,47 @@
      [:text {:class "wolf"  :x "750" :y "20"} "Wolf"]
      ]
 
+
     [:div#rainforest
      
      [:div#sky
 
       [:i {:class "fa fa-cloud fa-6x"
-           :style "left:130px; top:10px"}]
+           :style "left:5%; top:30px"}]
       
       [:i {:class "fa fa-cloud medium-cloud storm-cloud"
-           :style "left:230px; top:10px;"}]
+           :style "left:25%; top:50px;"}]
 
       [:i {:class "fa fa-cloud big-cloud"
-           :style "left:330px; top:30px;"}]
+           :style "left:60%; top:30px;"}]
       
       [:i {:class "fa fa-cloud"
-           :style "left:450px; font-size:75px; top:15px;"}]
+           :style "left:80%; font-size:75px; top:15px;"}]
       ]
 
      [:div#ground
 
       [:i {:class "fa fa-tree fa-5x"
-           :style "left:50px;top:-120px;"}
+           :style "left:10%;top:20px;"}
        ]
 
       [:div {:class "fa fa-tree"
-             :style "left:80px; top:-100px;font-size:160px"}]
+             :style "left:20%; top:-50px;font-size:160px"}]
       
       [:i {:class "fa fa-tree fa-5x"
-           :style "left:180px;top:-120px;"}
+           :style "left:35%;top:20px;"}
        ]
 
       [:i {:class "fa fa-tree fa-3x"
-           :style "left:220px;top:-120px;"}
+           :style "left:40%;top:20px;"}
        ]
 
       [:i {:class "fa fa-tree fa-4x"
-           :style "left:330px;top:-120px;"}
+           :style "left:50%;top:-20px;"}
        ]
 
       [:div {:class "fa fa-tree"
-             :style "left:400px; top:-110px;font-size:140px"}]
+             :style "left:60%; top:-50px;font-size:140px"}]
 
       ] ;; end of :div#ground
 
