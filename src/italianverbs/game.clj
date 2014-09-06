70bc81e39d0df7bfb05e0a7dfb35e1519a4fc37f
Eugene Koontz <ekoontz@hiro-tan.org>
2014-09-06 01:58:59 -0700

    formatting

--- src/italianverbs/game.clj
+++ src/italianverbs/game.clj
@@ -29,15 +29,27 @@
 
      [:div#ground
 
+      [:i {:class "fa fa-tree x2" :style "left:1%; top:-10%" }]
+
+      [:i {:class "fa fa-tree x1" :style "left:25%; top:5%" }]
+
+      [:i {:class "fa fa-tree x2" :style "left:50%; top:-35%" }
+       ]
+
+      [:i {:class "fa fa-tree x3" :style "left:75%;top:-50%" }
+       ]
+
+
       ] ;; end of :div#ground
 
      ] ;; end of :div#rainforest
 
     [:div#gameform
-     [:input {:id "game_input" :size "50"}]
-     [:button {:class "click"
-               :onclick "submit_game_response('game_input');"} "Answer" ]]
-       
+      [:input {:id "game_input" :size "50"}]
+      [:button {:class "click"
+                :onclick "submit_game_response('game_input');"} "Answer" ]
+      
+     ] ;; end of :div #gameform
 
     ] ;; end of :div #game
 ) ; html5/div
