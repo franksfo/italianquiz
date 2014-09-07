08a51b933ce9e85d2af2a856bd8df6f0867d5ca1
Eugene Koontz <ekoontz@hiro-tan.org>
2014-09-06 18:52:18 -0700

    add WARN and ERROR

--- resources/public/js/log4.js
+++ resources/public/js/log4.js
@@ -1,11 +1,16 @@
 // lib-ish:
+// TODO: maybe add FATAL or CRITICAL or something even worse than ERROR.
 var TRACE = 4;
 var DEBUG = 3;
 var INFO  = 2;
+var WARN  = 1;
+var ERROR  = 0;
 var logging = {
     4 : "TRACE",
     3 : "DEBUG",
-    2 : "INFO"
+    2 : "INFO",
+    1 : "WARN",
+    0 : "ERROR"
 };
 
 function log(level,str) {
