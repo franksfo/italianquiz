1dbdf1a040248a978200b2815ed38ba3f1f78535
Eugene Koontz <ekoontz@hiro-tan.org>
2014-09-07 20:35:17 -0700

    benchmark fix

--- benchmark.sh
+++ benchmark.sh
@@ -2,4 +2,4 @@
 # usage:  benchmark.sh
 # show only benchmark name and stats: ./benchmark.sh | egrep "\-\-"\|average\|stddev
 git log -1
-lein run -m italianverbs.benchmark/benchmark $*
+lein run -m italianverbs.benchmark/standard-benchmark $*
