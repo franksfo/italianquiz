befbf956aae1d476b21f377cb10432c69212346a
Eugene Koontz <ekoontz@hiro-tan.org>
2014-09-20 13:38:11 -0700

    working on mail

--- src/italianverbs/mail.clj
+++ src/italianverbs/mail.clj
@@ -7,11 +7,11 @@
 ;; build a message (can be used in unit tests or for various forms of delayed delivery)
 ;;
 ;; Please note that email/templates/warning.mustache should be on your classpath. For example, with Leiningen 2,
-;; you would use :resource-paths for this, like so: :resource-paths ["src/resources"]
-(build-email {:from "Joe The Robot", 
-              :to ["ops@megacorp.internal" "oncall@megacorp.internal"] 
-              :subject "OMG everything is down!"}
-             "email/warning.mustache" {:name "Joe"}
-             :text/plain)
-
-             
+;; email templates are stored in the top-level project.clj's :resource-paths, which is: "resources". 
+;; within that is 'email' which you can find in the second parameter below.
+(def my-cool-email-message
+  (build-email {:from "Joe The Robot", 
+                :to ["ops@megacorp.internal" "oncall@megacorp.internal"] 
+                :subject "OMG everything is down!"}
+               "email/warning.mustache" {:name "Joe"}
+               :text/plain))
