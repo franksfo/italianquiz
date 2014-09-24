e72e8bf701bec5bbfaa28746b6b82511df93db5e
Eugene Koontz <ekoontz@hiro-tan.org>
2014-09-23 22:31:29 -0700

    (defn foo []) :able to send mails via SMTP now

--- src/italianverbs/mail.clj
+++ src/italianverbs/mail.clj
@@ -1,6 +1,7 @@
 (ns italianverbs.mail
   (:require [clojurewerkz.mailer.core :refer [delivery-mode! with-settings with-defaults 
-                                              with-settings build-email deliver-email]]))
+                                              with-settings build-email deliver-email render
+                                              with-delivery-mode]]))
 
 (delivery-mode! :test)
 
@@ -9,16 +10,29 @@
 ;; Please note that email/templates/warning.mustache should be on your classpath. For example, with Leiningen 2,
 ;; email templates are stored in the top-level project.clj's :resource-paths, which is: "resources". 
 ;; within that is 'email' which you can find in the second parameter below.
-(def registration-email-message
+(def registration-email
   (build-email {:from "Verbcoach Registration", 
-                :to ["student@somewhere.edu"] 
-                :subject "Welcome to Verbcoach"}
-               "email/warning.mustache" {:name "Joe" :hash "abcdef12345678"}
+                :to ["ekoontz@hiro-tan.org"]
+                :subject "Verbcoach: Welcome! Please confirm your password."}
+               "email/warning.mustache" {:name "Joe" :authentication-hash "abcdef12345678"
+                                         :email "joe@foo.com"}
                :text/plain))
 
-(def registration-email-message
+(def reset-password-email
   (build-email {:from "Verbcoach Lost Password Recovery", 
                 :to ["student@somewhere.edu"] 
-                :subject "Reset your password"}
-               "email/warning.mustache" {:name "Joe" :hash "12345678abcdef"}
+                :subject "Verbcoach: Reset your password"}
+               "email/warning.mustache" {:name "Joe" :authentication-hash "12345678abcdef"}
                :text/plain))
+
+(defn send-test-mail []
+  (with-defaults { :foo "bar" :from "Joe The Robot <robot@megacorp.internal>" :subject "[Do Not Reply] Warning! Achtung! Внимание!" }
+    registration-email))
+
+(defn foo []
+  (with-delivery-mode :smtp
+    (deliver-email {:from "Joe The Robot", :to ["ekoontz@hiro-tan.org"] :subject "OMG everything is down!"}
+                   "email/warning.mustache" {:name "Joe" :host "mail.hiro-tan.org"})))
+
+
+
