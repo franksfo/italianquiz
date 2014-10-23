c6a84c680446d0cc5c44aba1a998391cb973c415
Eugene Koontz <ekoontz@hiro-tan.org>
2014-10-23 13:25:16 -0400

    slowly converthing italian lexicon

--- src/italianverbs/lexicon/italiano.clj
+++ src/italianverbs/lexicon/italiano.clj
@@ -2,7 +2,11 @@
   (:refer-clojure :exclude [get-in merge resolve]))
 
 (require '[italianverbs.pos :refer :all])
-(require '[italianverbs.unify :refer :all])
+(require '[italianverbs.unify :refer :all :exclude [unify]])
+(require '[italianverbs.unify :as unify])
+
+(defn unify [& args]
+  (apply unifyc args))
 
 (defn phonize [a-map a-string]
   (cond (or (vector? a-map) (seq? a-map))
@@ -16,6 +20,42 @@
 
 (def lexicon
   {
+
+   "a"
+   [(let [location (ref {:place true})]
+      {:synsem {:cat :prep
+                :sem {:pred :a
+                      :obj location
+                      :comparative false}
+                :subcat {:1 {:cat :noun
+                             :subcat '()
+                             :sem location}
+                         :2 '()}}})
+       {:synsem {:cat :prep
+             :sem {:pred :in}
+             :subcat {:1 {:cat :noun
+                          :sem {:city true}}}}}
+
+
+       (let [complement-semantics (ref {:pred :a
+                                        :mod {:pred :a}})]
+         {:synsem {:cat :prep
+                   :sem complement-semantics
+                   :subcat {:1 {:cat :verb
+                                :sem complement-semantics
+                                :infl :infinitive
+                                :subcat {:1 :top
+                                         :2 '()}}
+                            :2 '()}}})]
+
+   "acqua"
+   (unify (:agreement noun)
+          (:drinkable noun)
+          (:feminine noun)
+          {:synsem {:sem {:artifact false
+                          :animate false
+                          :pred :acqua}}})
+
 
    "cane"
    (unify agreement-noun
