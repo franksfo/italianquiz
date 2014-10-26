2bac0195854197cff5e0747c8634dfcfe30cb40f
Eugene Koontz <ekoontz@hiro-tan.org>
2014-10-26 11:16:01 -0400

    fix parsing test with regular future tense

--- test/italianverbs/test/parse.clj
+++ test/italianverbs/test/parse.clj
@@ -1,8 +1,14 @@
 (ns italianverbs.test.parse)
 
 (require '[clojure.test :refer :all])
-(require '[italianverbs.parse :refer :all])
-(require '[italianverbs.morphology :refer :all])
+(require '[italianverbs.parse :refer :all :exclude [fo fo-ps]])
+(require '[italianverbs.morphology :refer [fo]])
+(require '[italianverbs.morphology.italiano :refer :all])
 
-(deftest parse-test
-  (is (not (empty? (fo-ps (parse "un gatto" it-lexicon it-grammar))))))
+(deftest parse-test-1
+  (is (= "un gatto" (fo (parse "un gatto" it-lexicon it-grammar)))))
+
+(deftest parse-test-2
+  (is (contains? (set (map fo (parse "Antonio dormire" it-lexicon it-grammar))) "Antonio dormirà")))
+
+
