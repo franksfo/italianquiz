f957df55669a22408e837930c3fa88f07b31c7ee
Eugene Koontz <ekoontz@hiro-tan.org>
2014-10-26 11:02:14 -0400

    do not use language-specific (phonize): use load-time lexical transformations instead.

--- src/italianverbs/parse.clj
+++ src/italianverbs/parse.clj
@@ -24,9 +24,6 @@
 (def en-lexicon en-l/lexicon)
 (def it-lexicon it-l/lexicon)
 
-(def en-phonize en-l/phonize)
-(def it-phonize it-l/phonize)
-         
 (defn parse [input-string & [ lexicon grammar ] ]
   "return a list of all possible parse trees for the given list of tokens, given the lexicon and grammar."
   (if (or (nil? lexicon)
@@ -55,15 +52,22 @@
             true
             nil))))
 
+(defn do-lookup [input-string lexicon]
+  (let [tokens (str/split input-string #"[ ']")
+        looked-up (map #(lookup lexicon %)
+                       tokens)]
+    looked-up))
+  
+
+(defn dofoo [input-string & [lexicon grammar ] ]
+  (let [tokens (str/split input-string #"[ ']")
+        looked-up (map #(lookup lexicon %)
+                       tokens)]
+    (over/over it-grammar (get it-lexicon "Antonio") (get it-lexicon "dormire"))))
+
 (defn lookup [lexicon token]
   "return the subset set of lexemes that match this token from the lexicon."
-  (let [phonize-fn
-        (cond (= lexicon en-lexicon)
-              en-phonize
-              true
-              it-phonize)]
-    (if (get lexicon token)
-      (phonize-fn (get lexicon token) token))))
+  (get lexicon token))
 
 
 
