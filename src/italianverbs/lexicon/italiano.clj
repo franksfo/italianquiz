1d4eddd2bfbea4d8462036e1a4e5e1f87d697423
Eugene Koontz <ekoontz@hiro-tan.org>
2014-10-28 13:17:30 -0400

    re-add needed (phonize) function

--- src/italianverbs/lexicon/italiano.clj
+++ src/italianverbs/lexicon/italiano.clj
@@ -9,6 +9,17 @@
 (require '[italianverbs.unify :refer :all :exclude [unify]])
 (require '[italianverbs.unify :as unify])
 
+
+(defn phonize [a-map a-string]
+  (cond (or (vector? a-map) (seq? a-map))
+        (map (fn [each-entry]
+               (phonize each-entry a-string))
+             a-map)
+        true
+        (merge a-map
+               {:italiano a-string
+                :phrasal false})))
+
 (def lexicon
   {
 
