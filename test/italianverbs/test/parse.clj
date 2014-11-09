45828aed432123c67e4b699de09bdd15d3fde64d
Eugene Koontz <ekoontz@hiro-tan.org>
2014-11-09 13:18:20 -0800

    workaround for FIXME: bug in italianverbs.grammar.english (it uses the old lexicon most likely)

--- test/italianverbs/test/parse.clj
+++ test/italianverbs/test/parse.clj
@@ -1,13 +1,25 @@
 (ns italianverbs.test.parse)
 
 (require '[clojure.test :refer :all])
-(require '[italianverbs.parse :refer :all :exclude [fo fo-ps]])
+
+
+;; not yet needed, but might be some day.
+;(require '[italianverbs.generate :as gen])
+
+(require '[italianverbs.grammar.english :as en-g])
+(require '[italianverbs.grammar.italiano :as it-g])
+
+;(require '[italianverbs.lexicon.english :as en-l])
+(require '[italianverbs.lexicon.italiano :as it-l])
+;; not yet needed, but might be some day.
+;(require '[italianverbs.lexiconfn :as lexfn])
 (require '[italianverbs.morphology :refer [fo]])
-(require '[italianverbs.morphology.italiano :refer :all])
+
+(require '[italianverbs.parse :refer :all])
 
 (deftest parse-test-1
   (is (= "un gatto" (fo (parse "un gatto" it-lexicon it-grammar)))))
-
+ 
 (deftest parse-test-2
   (is (contains? (set (map fo (parse "Antonio dormire" it-lexicon it-grammar))) "Antonio dormirà")))
 
