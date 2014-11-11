e93d9345d62f06b8d265d05b1753622c7e9dc254
Eugene Koontz <ekoontz@hiro-tan.org>
2014-11-11 00:26:40 -0800

    figure out obscure keyword/seq problems by cleaning up requires in parse.clj

--- src/italianverbs/parse.clj
+++ src/italianverbs/parse.clj
@@ -2,32 +2,20 @@
  (:refer-clojure :exclude [get-in merge resolve find]))
 
 (require '[clojure.string :as str])
-
-;; not yet needed, but might be some day.
-;(require '[italianverbs.generate :as gen])
-
-(require '[italianverbs.grammar.english :as en-g])
 (require '[italianverbs.grammar.italiano :as it-g])
-
-(require '[italianverbs.lexicon.english :as en-l])
 (require '[italianverbs.lexicon.italiano :as it-l])
-;; not yet needed, but might be some day.
-;(require '[italianverbs.lexiconfn :as lexfn])
-
-(require '[italianverbs.lexicon.italiano :refer :all])
 (require '[italianverbs.morphology :refer (fo fo-ps)])
 (require '[italianverbs.morphology.italiano :refer (analyze get-string)])
-
 (require '[italianverbs.over :as over])
-(require '[italianverbs.unify :refer (get-in)])
 
-(declare lookup)
-
-(def en-grammar en-g/grammar)
 (def it-grammar it-g/grammar)
-
-(def en-lexicon en-l/lexicon)
 (def it-lexicon it-l/lexicon)
+
+(defn lookup [token & [lexicon]]
+  "return the subset of lexemes that match this token from the lexicon."
+  (let [lexicon (if lexicon lexicon it-lexicon)]
+    (analyze token (fn [k]
+                     (get lexicon k)))))
 
 (defn parse [input-string & [ lexicon grammar ]]
   "return a list of all possible parse trees for the given list of tokens, given the lexicon and grammar."
@@ -38,7 +26,7 @@
 
     ;; else, grammar and lexicon supplied:
     (let [tokens (str/split input-string #"[ ']")
-          looked-up (map #(lookup lexicon %)
+          looked-up (map #(lookup % lexicon)
                          tokens)]
       (cond (and (= (.size looked-up) 2)
                  (not (empty? (nth looked-up 0)))
@@ -60,15 +48,6 @@
             (over/over grammar
                        :top
                        (nth looked-up 1))
-            
+
             true
             nil))))
-
-(defn parse-both [input-string]
-  {:english (parse input-string en-lexicon en-grammar)
-   :italiano (parse input-string it-lexicon it-grammar)})
-  
-(defn lookup [lexicon token]
-  "return the subset of lexemes that match this token from the lexicon."
-  (analyze token (fn [k]
-                   (get lexicon k))))
