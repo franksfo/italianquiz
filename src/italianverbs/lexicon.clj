839293064884df8f2d51f2deb0a8bfdad5e0931d
Eugene Koontz <ekoontz@hiro-tan.org>
2014-11-17 00:06:25 -0800

    transitioning away from lex/* lexicon and using lexicon/* instead

--- src/italianverbs/lexicon.clj
+++ src/italianverbs/lexicon.clj
@@ -3,13 +3,10 @@
   (:require
    [clojure.set :refer (union)]
    [clojure.tools.logging :as log]
+   [italianverbs.lexicon.italiano :as italiano]
    [italianverbs.lexiconfn :refer (apply-multi-rules cache-serialization sem-impl subcat0 subcat1 transform)]
    ;; TODO: i.l.a_essere,i.l.esso_noi,i.l.notizie_potere,i.l.qualche_volte_volere are going away:
    ;; instead use i.l.italiano and i.l.english.
-   [italianverbs.lex.a_essere :refer :all]
-   [italianverbs.lex.esso_noi :refer :all]
-   [italianverbs.lex.notizie_potere :refer :all]
-   [italianverbs.lex.qualche_volta_volere :refer :all]
    [italianverbs.morphology :refer (fo)]
    [italianverbs.pos :refer :all]
    [italianverbs.unify :as unify]
@@ -47,7 +44,7 @@
                (set (lookup {:italiano {:infinitive italian}}))
                (set (lookup {:italiano {:infinitive {:infinitive italian}}}))
                (set (lookup {:italiano {:italiano italian}}))
-               (set (lookup {:italiano {:irregular {:passato italian}}})))]
+               (set (lookup {:italiano {:passato italian}})))]
     result))
 
 (defn en [english]
@@ -56,34 +53,16 @@
                (set (lookup {:english {:infinitive english}}))
                (set (lookup {:english {:infinitive {:infinitive english}}}))
                (set (lookup {:english {:english english}}))
-               (set (lookup {:english {:irregular {:passato english}}})))]
+               (set (lookup {:english {:passato english}})))]
     result))
 
 (clear!)
 
 (load "pos")
 
-(load "lex/a_essere")
-(load "lex/esso_noi")
-(load "lex/notizie_potere")
-(load "lex/qualche_volta_volere")
-
 (def lexicon
   ;; this filter is for debugging purposes to restrict lexicon to particular entries, if desired.
   ;; default shown is (not (nil? entry)) i.e. no restrictions except that an entry must be non-nil.
   ;;  (currently there is one nil below: "chiunque (anyone)").
-  (future
-    (filter (fn [entry]
-              (or (= entry :fail)
-                  (not (nil? entry))))
+  (future (flatten (vals italiano/lexicon))))
 
-            ;; TODO: move this fn to lexiconfn: keep any code out of the lexicon proper.
-            ;; this (map) adds, to each lexical entry, a copy of the serialized form of the entry.
-
-            (mapcat #(apply-multi-rules %)
-                    (map transform
-                         (concat
-                          a-essere
-                          esso-noi
-                          notizie-potere
-                          qualche_volta-volere))))))
