4440a129350726c2b825813567bc14d867a0ca97
Eugene Koontz <ekoontz@hiro-tan.org>
2014-11-23 08:50:42 -0800

    unifying bigrams and trigrams into ngrams

--- src/italianverbs/parse.clj
+++ src/italianverbs/parse.clj
@@ -35,14 +35,15 @@
        (create-bigram-map args (+ index 1) grammar)))
     {}))
 
-(defn create-trigram-map [args index grammar bigrams]
-  (if (< (+ 2 index) (.size args))
-    (let []
-      (log/debug (str "over(1): " (fo (get bigrams index))))
-      (log/debug (str "over(2): " (fo (get bigrams (+ 1 index)))))
-      (merge
-       {[index (+ 2 index)]
-        (lazy-cat
+(defn create-ngram-map [args index grammar & [bigrams]]
+  (let [bigrams (if bigrams bigrams (create-bigram-map args index grammar))]
+    (if (< (+ 2 index) (.size args))
+      (do
+        (log/debug (str "over(1): " (fo (get bigrams index))))
+        (log/debug (str "over(2): " (fo (get bigrams (+ 1 index)))))
+        (merge
+         {[index (+ 2 index)]
+          (lazy-cat
          (if (get bigrams [index (+ 1 index)])
            (over/over grammar 
                       (get bigrams [index (+ 1 index)])
@@ -51,14 +52,13 @@
            (over/over grammar 
                       (subvec args index (+ 1 index))
                       (get bigrams [(+ 1 index) (+ 2 index)]))))}
-       (create-trigram-map args (+ index 1) grammar bigrams)))
-    {}))
+       (create-ngram-map args (+ index 1) grammar bigrams)))
+      bigrams)))
 
 (defn parse-at [all & [ {length :length
-                         bigrams :bigrams
                          grammar :grammar
                          split-at :split-at
-                         trigrams :trigrams
+                         ngrams :ngrams
                          offset :offset}]]
   (if (and (> split-at 0)
            (< split-at length))
@@ -68,14 +68,12 @@
                        {:all all
                         :length split-at
                         :offset offset
-                        :bigrams bigrams
-                        :trigrams trigrams})
+                        :ngrams ngrams})
             right-side (parse
                         (subvec args split-at length)
                         {:all all
                          :offset (+ split-at offset)
-                         :bigrams bigrams
-                         :trigrams trigrams
+                         :ngrams ngrams
                          :left split-at
                          :length (- (.size args) split-at)
                          :right (.size args)})]
@@ -84,12 +82,12 @@
 
         (lazy-cat
          (cond (= length 2)
-               (let [retval (get bigrams [offset (+ 1 offset)])]
+               (let [retval (get ngrams [offset (+ 1 offset)])]
                  (do (if (not (empty? retval))
                        (log/debug (str " =2> " (fo retval))))
                      retval))
                (= length 3)
-               (let [retval (get trigrams [offset (+ 2 offset)])]
+               (let [retval (get ngrams [offset (+ 2 offset)])]
                  (do (if (not (empty? retval))
                        (log/debug (str " =3> " (fo retval))))
                      retval))
@@ -103,8 +101,7 @@
                      (log/info (str " =m> " (fo retval))))
                    retval)))
          (parse-at all {:offset offset
-                        :bigrams bigrams
-                        :trigrams trigrams
+                        :ngrams ngrams
                         :length length
                         :grammar grammar
                         :split-at (+ 1 split-at)})))))
@@ -112,20 +109,16 @@
 (defn parse [arg & [{all :all
                      offset :offset
                      length :length
-                     bigrams :bigrams
-                     trigrams :trigrams}]]
+                     ngrams :ngrams}]]
   "return a list of all possible parse trees for a string or a list of lists of maps (a result of looking up in a dictionary a list of tokens from the input string)"
   (let [offset (if offset offset 0)
         all (if all all (if (vector? arg)
                           arg))
         length (if length length (if (vector? arg)
                                    (.size arg)))
-        bigrams (if bigrams bigrams
-                    (if (vector? arg)
-                      (create-bigram-map arg 0 it-grammar)))
-        trigrams (if trigrams trigrams
+        ngrams (if ngrams ngrams
                      (if (vector? arg)
-                       (create-trigram-map arg 0 it-grammar bigrams)))]
+                       (create-ngram-map arg 0 it-grammar)))]
 
     (log/debug (str "parse: arg: " (fo arg)))
     (log/debug (str "parse: all: " (fo all)))
@@ -139,11 +132,10 @@
           (vector? arg)
           (let [result
                 (parse-at all {:offset offset
-                               :bigrams bigrams
                                :grammar it-grammar
                                :length length
                                :split-at 1
-                               :trigrams trigrams})]
+                               :ngrams ngrams})]
             (do
               (if (not (empty? result))
                 (log/debug (str "parse: " (str/join " + "
