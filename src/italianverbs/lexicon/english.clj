b9091f86bd742c0de1ebb6329da0bea73f7e60a5
Eugene Koontz <ekoontz@hiro-tan.org>
2014-11-25 12:47:28 -0800

    add the verb “to read”

--- src/italianverbs/lexicon/english.clj
+++ src/italianverbs/lexicon/english.clj
@@ -1,18 +1,7 @@
-(ns italianverbs.lexicon.english
-  (:refer-clojure :exclude [get-in merge resolve]))
+(ns italianverbs.lexicon.english)
 
+(require '[italianverbs.lexiconfn :refer (compile-lex unify)])
 (require '[italianverbs.pos :refer :all])
-(require '[italianverbs.unify :refer :all])
-
-(defn phonize [a-map a-string]
-  (cond (or (vector? a-map) (seq? a-map))
-        (map (fn [each-entry]
-               (phonize each-entry a-string))
-             a-map)
-        true
-        (merge a-map
-               {:english a-string
-                :phrasal false})))
 
 (def lexicon
   {"a"
@@ -41,6 +30,25 @@
           {:synsem {:sem {:pred :abbracciare
                           :subj {:human true}
                           :obj {:living true}}}})
+
+   "read"
+   (let [common {:english {:english "to read" ;; spelled "read" but pronounced like "reed".
+                           :past "read"
+                           :note "(past)"} ;; spelled "read" but pronounced like "red".
+                 :synsem {:sem {:pred :leggere
+                                :discrte false
+                                :subj {:human true}}}}]
+                 
+     [(unify
+       common
+       transitive
+       {:synsem {:sem {:obj {:legible true}}}})
+
+      (unify
+       common
+       intransitive-unspecified-obj)])
+   
+
 })
 
 
