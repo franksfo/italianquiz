8f011c4751892ca5887716490cb8efb80d2c10ce
Eugene Koontz <ekoontz@hiro-tan.org>
2014-11-26 10:52:22 -0800

    make parse calls language specific by specifying a grammar and a lexicon

--- test/italianverbs/test/parse.clj
+++ test/italianverbs/test/parse.clj
@@ -17,6 +17,16 @@
 (require '[italianverbs.parse :refer :all])
 (require '[italianverbs.unify :refer (get-in)])
 
+(deftest tokenization-1
+  "there should be only 2 tokens, even though there's 3 tokens at first according to initial tokenization."
+  (let [result (toks "la sua birra" it/lexicon)]
+    (is (= (.size result) 2))))
+
+(deftest tokenization-1
+  "there should be 3 tokens, for each of the tokens according to initial tokenization (there is no way to combine any initial tokens in to larger tokens as there was in the test immediately above."
+  (let [result (toks "il gatto nero" it/lexicon)]
+    (is (= (.size result) 3))))
+
 (deftest parse-test-1
   (is (= "un gatto" (fo (first (parse "un gatto" it/lexicon it-g/grammar))))))
  
@@ -24,17 +34,17 @@
   (is (= "a cat" (fo (first (parse "a cat" en/lexicon en-g/grammar))))))
 
 (deftest parse-test-2
-  (is (= (fo (first (parse "Antonio dormirà")))
+  (is (= (fo (first (parse "Antonio dormirà" it/lexicon it-g/grammar)))
          "Antonio dormirà")))
 
 (deftest parse-test-3
-  (let [result (parse "il gatto nero")]
+  (let [result (parse "il gatto nero" it/lexicon it-g/grammar)]
     (is (> (.size result) 0))
     (is (= (get-in (first result) [:synsem :sem :pred])
            :gatto))))
 
 (deftest parse-test-4
-  (let [result (parse "il gatto nero dorme")]
+  (let [result (parse "il gatto nero dorme" it/lexicon it-g/grammar)]
     (is (> (.size result) 0))
     (is (= (get-in (first result) [:synsem :sem :pred])
            :dormire))
@@ -42,17 +52,5 @@
            :gatto))
     (is (= (get-in (first result) [:synsem :sem :subj :mod :pred])
            :nero))))
-
-(deftest tokenization-1
-  "there should be only 2 tokens, even though there's 3 tokens at first according to initial tokenization."
-  (let [result (toks "la sua birra")]
-    (is (= (.size result) 2))))
-
-(deftest tokenization-1
-  "there should be 3 tokens, for each of the tokens at first according to initial tokenization."
-  (let [result (toks "il gatto nero")]
-    (is (= (.size result) 3))))
-
-
 
 
