1dca29dc0ce524f387ded518fc37ec28885a5874
Eugene Koontz <ekoontz@hiro-tan.org>
2014-11-28 10:44:20 -0800

    add language-specific morphology and generation functionality

--- src/italianverbs/english.clj
+++ src/italianverbs/english.clj
@@ -2,10 +2,14 @@
   (:refer-clojure :exclude [get-in])
   (:require 
    [italianverbs.grammar.english :as gram]
-   [italianverbs.lexicon.english :as lex]))
+   [italianverbs.lexicon.english :as lex]
+   [italianverbs.morphology.english :as morph]))
 
 (def generate gram/generate)
+(def get-string morph/get-string)
 (def grammar gram/grammar)
 (def lexicon lex/lexicon)
 (def lookup lex/lookup)
 (def parse gram/parse)
+(def sentence gram/sentence)
+
--- src/italianverbs/italiano.clj
+++ src/italianverbs/italiano.clj
@@ -2,11 +2,14 @@
   (:refer-clojure :exclude [get-in])
   (:require 
    [italianverbs.grammar.italiano :as gram]
-   [italianverbs.lexicon.italiano :as lex]))
+   [italianverbs.lexicon.italiano :as lex]
+   [italianverbs.morphology.italiano :as morph]))
 
 (def generate gram/generate)
+(def get-string morph/get-string)
 (def grammar gram/grammar)
 (def lexicon lex/lexicon)
 (def lookup lex/lookup)
 (def parse gram/parse)
+(def sentence gram/sentence)
 
