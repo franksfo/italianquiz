4409750212409e7e95ff438a07a9f05bca99accf
Eugene Koontz <ekoontz@hiro-tan.org>
2014-12-18 22:49:30 -0800

    remove references to generate

--- src/italianverbs/grammar/english.clj
+++ src/italianverbs/grammar/english.clj
@@ -3,7 +3,6 @@
   (:require 
    [clojure.tools.logging :as log]
    [italianverbs.cache :refer (build-lex-sch-cache create-index over spec-to-phrases)]
-   [italianverbs.generate :as generate]
    [italianverbs.lexicon.english :as en-lex]
    [italianverbs.morphology :refer (fo fo-ps)]
    [italianverbs.parse :as parse]
