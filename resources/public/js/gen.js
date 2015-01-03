25a3e7c9d74034c5f59093a9d1a28515392bfd53
Eugene Koontz <ekoontz@hiro-tan.org>
2015-01-02 18:23:24 -0800

    remove gen_from_verb() since it’s only used in one place, in a call from gen_per_verb

--- resources/public/js/gen.js
+++ resources/public/js/gen.js
@@ -22,106 +22,102 @@ function gen_per_verb(prefix) {
 	verb_rows = get_verb_rows_by_prefix(prefix);
     }
 
-    // now call gen_from_verb() for each verb row, which will fill in all of the <td>s.
+    // now we know what rows to work on - fill in each of them.
     verb_rows.each(function() {
 	var verb_dom_id = this.id;
 	var verb = verb_dom_id;
 	var re = new RegExp("^" + prefix);
 	verb = verb.replace(re,"");
-	    verb = verb.replace(/^verb_/,"");
-	gen_from_verb(verb,prefix);
-    });
-}
-
-function gen_from_verb(verb,prefix) {
-    log(INFO,"gen_from_verb(" + verb + "," + prefix + ");");
-
-    // not sure why or if this is necessary..??
-    var re = new RegExp("^" + prefix);
-    verb = verb.replace(re,"");
-    verb = verb.replace(/^verb_/,"");
-
-    spec = sentence_with_tense_info(source_language,verb);
-    var serialized_spec = encodeURIComponent(JSON.stringify(spec));
-
-    $.ajax({
-	cache: false,
-	dataType: "html",
-	url: "/engine/generate?lang=" + source_language + "&model=" + source_language_model + "&spec="+serialized_spec,
-	success: generate_with_verb
-    });
-
-    function generate_with_verb(content) {
-	// 1. The server sent us an example sentence: parse the response into a JSON object.
-	var evaluated = jQuery.parseJSON(content);
-	var response = evaluated[source_language];
-	var spec = evaluated.spec;
-	var pred = spec["synsem"]["sem"]["pred"];
-
-	var semantics = evaluated.semantics;
-	if (response == "") {
-	    response = "<i class='fa fa-times-circle'> </i>";
-	}
-
-	var serialized_spec = encodeURIComponent(JSON.stringify({"synsem": {"sem": semantics}}));
-
-
-	// 2. Now that we have the example sentence in the source language in the variable _response_: now, paste it in to the DOM tree in the right place:
-	$("#"+prefix+"verb_"+verb).html("<a href='/engine/generate?" + 
-					"&spec=" + serialized_spec + 
-					"&lang=" + source_language + 
-					"&model=" + source_language_model + 
-					"&debug=true" +
-					"'>" + response + "</a>");
-
-	// reload link:
-	$("#"+prefix+"reload_"+verb).attr("onclick","javascript:refresh_row('" + verb + "','" + prefix + "');return false;");
-
-	var infinitive_spec = {"synsem": {"cat": "verb",
-					  "sem": {"pred": verb},
-					  "infl": "infinitive"}};
-
-	var serialized_infinitive_spec = encodeURIComponent(JSON.stringify(infinitive_spec));
-
+	verb = verb.replace(/^verb_/,"");
+	
+	log(INFO,"gen_from_verb(" + verb + "," + prefix + ");");
+	
+	// not sure why or if this is necessary..??
+	var re = new RegExp("^" + prefix);
+	verb = verb.replace(re,"");
+	verb = verb.replace(/^verb_/,"");
+	
+	spec = sentence_with_tense_info(source_language,verb);
+	var serialized_spec = encodeURIComponent(JSON.stringify(spec));
+	
 	$.ajax({
 	    cache: false,
 	    dataType: "html",
-	    url: "/engine/lookup?lang=" + target_language + "&spec=" + serialized_infinitive_spec,
-	    success: translate_from_semantics
+	    url: "/engine/generate?lang=" + source_language + "&model=" + source_language_model + "&spec="+serialized_spec,
+	    success: generate_with_verb
 	});
 
-	function translate_from_semantics(content) {
-	    evaluated = jQuery.parseJSON(content);
-	    var response;
-	    if (evaluated.response == "") {
-		// could not translate: show a link with an error icon (fa-times-circle)
-		response = "<a href='/engine/lookup?lang="+ target_language + "&spec=" + serialized-spec + "'>" +
-		    "<i class='fa fa-times-circle'> </i>" + " </a>";
-	    } else {
-
-		response = evaluated[target_language];
+	function generate_with_verb(content) {
+	    // 1. The server sent us an example sentence: parse the response into a JSON object.
+	    var evaluated = jQuery.parseJSON(content);
+	    var response = evaluated[source_language];
+	    var spec = evaluated.spec;
+	    var pred = spec["synsem"]["sem"]["pred"];
+	    
+	    var semantics = evaluated.semantics;
+	    if (response == "") {
+	    response = "<i class='fa fa-times-circle'> </i>";
 	    }
-	    $("#"+prefix+"target_verb_"+pred).html(response);
-
-	    var generate_target_language_url = "/engine/generate?lang="+ target_language + "&model=" + 
-		target_language_model + "&spec=" + serialized_spec;
-
+	    
+	    var serialized_spec = encodeURIComponent(JSON.stringify({"synsem": {"sem": semantics}}));
+	    
+	    // 2. Now that we have the example sentence in the source language in the variable _response_: now, paste it in to the DOM tree in the right place:
+	    $("#"+prefix+"verb_"+verb).html("<a href='/engine/generate?" + 
+					    "&spec=" + serialized_spec + 
+					    "&lang=" + source_language + 
+					    "&model=" + source_language_model + 
+					    "&debug=true" +
+					    "'>" + response + "</a>");
+	    
+	    // reload link:
+	    $("#"+prefix+"reload_"+verb).attr("onclick","javascript:refresh_row('" + verb + "','" + prefix + "');return false;");
+	    
+	    var infinitive_spec = {"synsem": {"cat": "verb",
+					      "sem": {"pred": verb},
+					  "infl": "infinitive"}};
+	    
+	    var serialized_infinitive_spec = encodeURIComponent(JSON.stringify(infinitive_spec));
+	    
 	    $.ajax({
 		cache: false,
 		dataType: "html",
-		url: generate_target_language_url,
-		success: function translate(content) {
-		    var evaluated  = jQuery.parseJSON(content);
-		    var response = evaluated[target_language];
-		    if (response == "") {
-			// could not generate anything: show a link with an error icon (fa-times-circle)
-			response = "<i class='fa fa-times-circle'> </i>";
-		    }
-		    $("#"+prefix+"target_translation_"+pred).html("<a href='" + generate_target_language_url + "'>" + response + "</a>");
-		}
+	    url: "/engine/lookup?lang=" + target_language + "&spec=" + serialized_infinitive_spec,
+		success: translate_from_semantics
 	    });
+	    
+	    function translate_from_semantics(content) {
+		evaluated = jQuery.parseJSON(content);
+		var response;
+		if (evaluated.response == "") {
+		    // could not translate: show a link with an error icon (fa-times-circle)
+		response = "<a href='/engine/lookup?lang="+ target_language + "&spec=" + serialized-spec + "'>" +
+			"<i class='fa fa-times-circle'> </i>" + " </a>";
+		} else {
+		    
+		    response = evaluated[target_language];
+		}
+		$("#"+prefix+"target_verb_"+pred).html(response);
+
+		var generate_target_language_url = "/engine/generate?lang="+ target_language + "&model=" + 
+		    target_language_model + "&spec=" + serialized_spec;
+		
+		$.ajax({
+		    cache: false,
+		    dataType: "html",
+		    url: generate_target_language_url,
+		    success: function translate(content) {
+			var evaluated  = jQuery.parseJSON(content);
+			var response = evaluated[target_language];
+			if (response == "") {
+			    // could not generate anything: show a link with an error icon (fa-times-circle)
+			    response = "<i class='fa fa-times-circle'> </i>";
+			}
+			$("#"+prefix+"target_translation_"+pred).html("<a href='" + generate_target_language_url + "'>" + response + "</a>");
+		    }
+		});
+	    }
 	}
-    }
+    });
 }
 
 function refresh_row(verb,prefix) {
