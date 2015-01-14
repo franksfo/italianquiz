6b445de5d10602e7a9d07384bc0b11bd9a32521e
Eugene Koontz <ekoontz@hiro-tan.org>
2015-01-13 23:32:58 -0800

    add some requires

--- test/italianverbs/test/tour.clj
+++ test/italianverbs/test/tour.clj
@@ -1,8 +1,11 @@
 (ns italianverbs.test.tour
   (:require
    [clojure.test :refer :all]
+   [italianverbs.engine :refer [generate]]
    [italianverbs.italiano :as it]
+   [italianverbs.morphology :refer [fo]]
    [italianverbs.tour :refer :all]
+   [italianverbs.unify :refer (unify unifyc)]
    [korma.core :as k]))
 
 ;; TODO: need a shim to return fixtures (simulated database results)
