68d56f4a438e53f1015d18e6acd532cfff1f5ec1
Eugene Koontz <ekoontz@hiro-tan.org>
2015-01-24 13:29:37 -0800

    working on implementing same API that italianverbs/engine does, but simply using stored sentences rather than dynamically generating.

--- src/italianverbs/borges.clj
+++ src/italianverbs/borges.clj
@@ -1,2 +1,31 @@
 
 ;; SELECT * FROM (SELECT synsem->'sem'->'pred'::text AS pred,surface FROM english) AS en WHERE en.pred='"andare"';
+;; SELECT it.surface  FROM (SELECT synsem->'sem'->'pred' AS pred,surface,synsem FROM italiano) AS it WHERE it.synsem->'sem' @> '{"pred":"andare"}';
+
+;; SELECT surface FROM italiano WHERE synsem->'sem' @> '{"pred":"andare"}';
+
+;;SELECT italiano.surface,english.surface FROM italiano INNER JOIN english ON italiano.synsem->'sem' = english.synsem->'sem';
+
+;; number of distinct english <-> italiano translation pairs
+;; SELECT count(*) FROM (SELECT DISTINCT english.surface AS en, italiano.surface AS it FROM italiano INNER JOIN english ON italiano.synsem->'sem' = english.synsem->'sem' ORDER BY english.surface) AS foo;
+(ns italianverbs.borges)
+
+(declare lookup)
+(declare generate-from-request)
+(declare resolve-model)
+
+(def routes
+  (compojure/routes
+   (GET "/lookup" request
+       (lookup request))
+
+  (GET "/generate" request
+       (generate-from-request request))))
+
+(defn generate-from-request [request]
+  "respond to an HTTP client's request with a generated sentence, given the client's desired spec, language name, and language model name."
+)
+
+(defn generate [spec language-model]
+  "generate a sentence matching 'spec' given the supplied language model.")
+
