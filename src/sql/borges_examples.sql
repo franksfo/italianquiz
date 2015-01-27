0d2e1cb320e4df7a9d30a987183d004822ef9729
Eugene Koontz <ekoontz@hiro-tan.org>
2015-01-27 00:21:21 -0800

    working db queries about equivalent meanings

--- src/sql/borges_examples.sql
+++ src/sql/borges_examples.sql
@@ -5,3 +5,15 @@ SELECT DISTINCT * FROM (SELECT english.surface AS en, italiano.surface AS it
                                                ON italiano.language = 'it' 
                                               AND english.language = 'en'
                                               AND italiano.structure->'synsem'->'sem' = english.structure->'synsem'->'sem') AS pairs;
+
+
+-- examples where there is no English equivalent (an erroroneous situation)
+     SELECT italiano.surface,english.surface 
+       FROM expression AS italiano 
+  LEFT JOIN expression AS english 
+         ON italiano.structure->'synsem'->'sem' = english.structure->'synsem'->'sem' 
+        AND italiano.language = 'it' 
+        AND english.language='en'
+      WHERE english IS NULL AND italiano.language='it';
+
+
