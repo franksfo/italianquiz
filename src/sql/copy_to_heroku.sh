9784eb7bc380c44728f3f5f12a13ba7ecc2fffba
Eugene Koontz <ekoontz@hiro-tan.org>
2015-02-17 00:32:02 -0800

    update heroku db location

--- src/sql/copy_to_heroku.sh
+++ src/sql/copy_to_heroku.sh
@@ -1,3 +1,3 @@
 echo "TRUNCATE TABLE verb" | heroku pg:psql --app heroku-postgres-712bd6ba crimson
 echo "TRUNCATE TABLE vgroup" | heroku pg:psql --app heroku-postgres-712bd6ba crimson
-pg_dump -U verbcoach verbcoach | heroku pg:psql --app heroku-postgres-712bd6ba crimson
+pg_dump -U verbcoach verbcoach | heroku pg:psql --app heroku pg:psql --app verbcoach HEROKU_POSTGRESQL_GOLD 
