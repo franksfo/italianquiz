2dabe489b9b86b3c0b73ed40b91321f1ba3f78d1
Eugene Koontz <ekoontz@hiro-tan.org>
2015-03-08 23:30:50 -0700

    two possible cities for Spanish: Barcelona and Mexico DF

--- resources/public/js/es.js
+++ resources/public/js/es.js
@@ -1,5 +1,10 @@
-// Barcelona is defined in cities.js.
+// Barcelona and Mexico_DF are defined in cities.js.
 var tour_path = Barcelona;
+
+var tour_paths = {
+    "ES": Barcelona,
+    "MX": Mexico_DF
+};
 
 var encouragements = [
     "Bueno!",
