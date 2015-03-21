e07a0edd7096bad39441aa044285ce0339ccf4a2
Eugene Koontz <ekoontz@hiro-tan.org>
2015-03-21 10:27:48 -0700

    SQL using containment to show equivalence pairs

--- src/sql/borges_examples.sql
+++ src/sql/borges_examples.sql
@@ -1,4 +1,4 @@
--- number of distinct english <-> italiano translation pairs
+-- distinct english <-> italiano translation pairs
 SELECT DISTINCT * FROM (SELECT english.surface AS en, 
                               italiano.surface AS it,
                               italiano.structure->'synsem'->'sem'->'subj'->'pred' AS subject_it,
@@ -11,6 +11,39 @@ SELECT DISTINCT * FROM (SELECT english.surface AS en,
                                               AND english.language = 'en'
                                               AND italiano.structure->'synsem'->'sem' = english.structure->'synsem'->'sem') AS pairs;
 
+-- distinct espanol <-> italiano translation pairs
+SELECT DISTINCT * FROM (SELECT english.surface AS en, 
+                               espanol.surface AS es,
+                               espanol.structure->'synsem'->'sem'->'subj'->'pred' AS subject_es,
+                               espanol.structure->'synsem'->'sem'->'pred' AS pred_es,
+                                espanol.structure->'synsem'->'sem'->'subj'->'pred' AS subject_es,
+                               espanol.structure->'synsem'->'sem'->'pred' AS pred_es,
+                              english.structure->'synsem'->'sem',
+                              espanol.structure->'synsem'->'sem'
+                                              FROM expression AS espanol 
+                                        INNER JOIN expression AS english 
+                                                ON espanol.language = 'es' 
+                                               AND english.language = 'en'
+                                               AND espanol.structure->'synsem'->'sem' = english.structure->'synsem'->'sem') AS pairs;
+
+-- same but containment:
+-- distinct english <-> italiano translation pairs
+SELECT DISTINCT * FROM (SELECT english.surface AS en, 
+                              italiano.surface AS it
+                                             FROM expression AS italiano 
+                                       INNER JOIN expression AS english 
+                                               ON italiano.language = 'it' 
+                                              AND english.language = 'en'
+                                              AND (italiano.structure->'synsem'->'sem') @> (english.structure->'synsem'->'sem')) AS pairs;
+
+-- distinct espanol <-> italiano translation pairs
+SELECT DISTINCT * FROM (SELECT english.surface AS en, 
+                               espanol.surface AS es,
+                                              FROM expression AS espanol 
+                                        INNER JOIN expression AS english 
+                                                ON espanol.language = 'es' 
+                                               AND english.language = 'en'
+                                               AND (espanol.structure->'synsem'->'sem') @> (english.structure->'synsem'->'sem')) AS pairs;
 
 -- examples where there is no English equivalent (an erroroneous situation)
      SELECT italiano.surface,english.surface 
