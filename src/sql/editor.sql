6b5d02cdd3861a54ba51460ceb218a8fe9554f03
Eugene Koontz <ekoontz@hiro-tan.org>
2015-03-21 17:24:40 -0700

    + translation_select table

--- src/sql/editor.sql
+++ src/sql/editor.sql
@@ -21,3 +21,18 @@ CREATE TABLE inflections_per_game (game bigint REFERENCES games(id),inflection t
 
 ALTER TABLE games ADD COLUMN target text;
 ALTER TABLE games ADD COLUMN source text;
+
+CREATE TABLE translation_select (id bigint NOT NULL);
+ALTER TABLE translation_select ADD CONSTRAINT select_pkey PRIMARY KEY (id);
+CREATE SEQUENCE translation_select_id_seq
+                     START WITH 1
+                     INCREMENT BY 1
+                     NO MINVALUE
+                     NO MAXVALUE
+                     CACHE 1;
+ALTER TABLE ONLY translation_select ALTER COLUMN id SET DEFAULT nextval('translation_select_id_seq'::regclass);
+
+ALTER TABLE translation_select ADD COLUMN source TEXT;
+ALTER TABLE translation_select ADD COLUMN target TEXT;
+ALTER TABLE translation_select ADD COLUMN source_spec JSONB; 
+ALTER TABLE translation_select ADD COLUMN target_spec JSONB; 
