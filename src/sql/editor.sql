008da8f726b1c448ef8c9c01a50da59e7f4e601c
Eugene Koontz <ekoontz@hiro-tan.org>
2015-03-29 12:01:53 -0700

    editor-related SQL

--- src/sql/editor.sql
+++ src/sql/editor.sql
@@ -26,35 +26,6 @@ CREATE SEQUENCE game_id_seq
 ALTER TABLE ONLY game ALTER COLUMN id SET DEFAULT nextval('game_id_seq'::regclass);
 ALTER TABLE ONLY game ADD CONSTRAINT game_pkey PRIMARY KEY (id);
 
-INSERT INTO or_group (name,selects) VALUES ('Useful Spanish Verbs',
-       ARRAY['{"espanol":{"espanol": "ayudar" }}'::jsonb,
-             '{"espanol":{"espanol": "comer" }}'::jsonb,
-	     -- ... etc .. add more verbs here..
-             '{"espanol":{"espanol": "hablar" }}'::jsonb]);
-
-INSERT INTO or_group (name,selects) VALUES ('Present tense',
-                                            ARRAY['{"synsem":{"sem":{"infl":"tense"}}}'::jsonb]);
-
-INSERT INTO or_group (name,selects) VALUES ('Empty','{}');
-
-INSERT INTO game (name,source_set,target_set,source,target) 
-     SELECT 'The Useful Spanish game', ARRAY[source.id],ARRAY[target.id],'en','es' 
-       FROM or_group AS source
- INNER JOIN or_group AS target
-         ON source.name = 'Empty'
-        AND (target.name = 'Useful Spanish Verbs'
-         OR  target.name = 'Present tense');
-
--- Get selects for all or_groups for the game called 'The Useful Spanish game':
--- 
-    SELECT source.selects AS source,target.selects AS target
-      FROM game 
-INNER JOIN or_group AS source
-        ON source.id = ANY(game.source_set) 
-INNER JOIN or_group AS target
-        ON target.id = ANY(game.target_set) 
-     WHERE game.name='The Useful Spanish game';
-
 CREATE SEQUENCE city_id_seq
     START WITH 1
     INCREMENT BY 1
@@ -73,29 +44,4 @@ ALTER TABLE ONLY city ADD CONSTRAINT city_pkey PRIMARY KEY (id);
 CREATE TABLE city_game (
     game bigint REFERENCES game(id),
     city bigint REFERENCES city(id));
-
-INSERT INTO city (name,country) VALUES ('Firenze','it');
-INSERT INTO city (name,country) VALUES ('Barcelona','es');
-INSERT INTO city (name,country) VALUES ('Mexico, D.F.','mx');
-
---INSERT INTO city_game
-TRUNCATE city_game;
-
-INSERT INTO city_game (city,game) 
-      SELECT city.id AS city,game.id AS game 
-        FROM game 
-  INNER JOIN city 
-          ON (game.source = 'en' AND game.target='it' AND city.country='it');
-
-INSERT INTO city_game (city,game) 
-      SELECT city.id AS city,game.id AS game 
-        FROM game 
-  INNER JOIN city 
-          ON (game.source = 'en' AND game.target='es' AND city.country='es');
-
-INSERT INTO city_game (city,game) 
-      SELECT city.id AS city,game.id AS game 
-        FROM game 
-  INNER JOIN city 
-          ON (game.source = 'en' AND game.target='es' AND city.country='mx');
 
---
+++ src/sql/editor_insert.sql
@@ -0,0 +1,58 @@
+TRUNCATE or_group;
+
+INSERT INTO or_group (name,selects) VALUES ('Useful Spanish Verbs',
+       ARRAY['{"espanol":{"espanol": "ayudar" }}'::jsonb,
+             '{"espanol":{"espanol": "comer" }}'::jsonb,
+	     -- ... etc .. add more verbs here..
+             '{"espanol":{"espanol": "hablar" }}'::jsonb]);
+
+INSERT INTO or_group (name,selects) VALUES ('Present tense',
+                                            ARRAY['{"synsem":{"sem":{"infl":"tense"}}}'::jsonb]);
+
+INSERT INTO or_group (name,selects) VALUES ('Empty','{}');
+
+TRUNCATE game;
+
+INSERT INTO game (name,source_set,target_set,source,target) 
+     SELECT 'The Useful Spanish game', ARRAY[source.id],ARRAY[target.id],'en','es' 
+       FROM or_group AS source
+ INNER JOIN or_group AS target
+         ON source.name = 'Empty'
+        AND (target.name = 'Useful Spanish Verbs'
+         OR  target.name = 'Present tense');
+
+-- Get selects for all or_groups for the game called 'The Useful Spanish game':
+-- 
+    SELECT source.selects AS source,target.selects AS target
+      FROM game 
+INNER JOIN or_group AS source
+        ON source.id = ANY(game.source_set) 
+INNER JOIN or_group AS target
+        ON target.id = ANY(game.target_set) 
+     WHERE game.name='The Useful Spanish game';
+
+INSERT INTO city (name,country) VALUES ('Firenze','it');
+INSERT INTO city (name,country) VALUES ('Barcelona','es');
+INSERT INTO city (name,country) VALUES ('Mexico, D.F.','mx');
+
+--INSERT INTO city_game
+TRUNCATE city_game;
+
+INSERT INTO city_game (city,game) 
+      SELECT city.id AS city,game.id AS game 
+        FROM game 
+  INNER JOIN city 
+          ON (game.source = 'en' AND game.target='it' AND city.country='it');
+
+INSERT INTO city_game (city,game) 
+      SELECT city.id AS city,game.id AS game 
+        FROM game 
+  INNER JOIN city 
+          ON (game.source = 'en' AND game.target='es' AND city.country='es');
+
+INSERT INTO city_game (city,game) 
+      SELECT city.id AS city,game.id AS game 
+        FROM game 
+  INNER JOIN city 
+          ON (game.source = 'en' AND game.target='es' AND city.country='mx');
+
