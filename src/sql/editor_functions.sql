e8cd0594540aa4d2e4bf13aab4d3818c2142b3cf
Eugene Koontz <ekoontz@hiro-tan.org>
2015-04-01 23:56:34 -0700

    add array_sort_unique() function, useful in conjuction with array_agg()

--- src/sql/editor_functions.sql
+++ src/sql/editor_functions.sql
@@ -5,7 +5,7 @@
 CREATE OR REPLACE FUNCTION expressions_for_game(game_id int) 
         RETURNS TABLE (
     source text, target text) AS 
-$$
+$body$
 BEGIN
 RETURN QUERY
 
@@ -54,7 +54,15 @@ RETURN QUERY
 
 END;
 
-$$ LANGUAGE plpgsql;
+$body$ LANGUAGE plpgsql;
 
-
-
+-- thanks to http://stackoverflow.com/questions/3994556/eliminate-duplicate-array-values-in-postgres
+CREATE OR REPLACE FUNCTION array_sort_unique (ANYARRAY) RETURNS ANYARRAY
+LANGUAGE SQL
+AS $body$
+  SELECT ARRAY(
+    SELECT DISTINCT $1[s.i]
+    FROM generate_series(array_lower($1,1), array_upper($1,1)) AS s(i)
+    ORDER BY 1
+  );
+$body$;
